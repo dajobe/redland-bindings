211f8fcd9c68ba5f015930416855c42249f35480
Dave Beckett <dave@dajobe.org>
2000-10-26 21:39:25 +0000

    Updated to have parsing too. Tidied message output

--- perl/example.pl
+++ perl/example.pl
@@ -76,31 +76,60 @@
 
 use RDF;
 
+$test_file="dc.rdf";
+
+warn "Creating storage\n";
 my $storage=new RDF::Storage("hashes", "test", 
 			     "new='yes',hash-type='bdb',dir='.'");
 die "Failed to create RDF::Storage\n" unless $storage;
   
+warn "\nCreating model\n";
 my $model=new RDF::Model($storage, "");
 die "Failed to create RDF::Model for storage\n" unless $model;
 
 
+warn "\nCreating statement\n";
 my $statement=RDF::Statement->new_from_nodes(RDF::Node->new_from_uri_string("http://purl.org/net/dajobe/"),
 					     RDF::Node->new_from_uri_string("http://purl.org/dc/elements/1.1/creator"),
-					     RDF::Node->new_from_literal("Dave Beckett", undef, 0, 0));
-
+					     RDF::Node->new_from_literal("Dave Beckett", "", 0, 0));
 die "Failed to create RDF::Statement\n" unless $statement;
 
+warn "\nAdding statement to model\n";
 # after this $statement is owned by $model and should not be used
 $model->add_statement($statement);
-undef $statement;
+$statement=undef;
 
-$statement=new RDF::Statement;
+warn "\nParsing URI (file) $test_file\n";
+my $uri=new RDF::URI("file:$test_file");
+
+my $parser=new RDF::Parser('repat');
+die "Failed to find parser\n" if !$parser;
+
+$stream=$parser->parse_as_stream($uri,$uri);
+my $count=0;
+while(!$stream->end) {
+  $model->add_statement($stream->next);
+  $count++;
+}
+$stream=undef;
+warn "Parsing added $count statements\n";
+
+warn "\nPrinting all statements\n";
+$stream=$model->serialise;
+while(!$stream->end) {
+  print "Statement: ",$stream->next->as_string,"\n";
+}
+$stream=undef;
+
+warn "\nSearching model for statements matching predicate http://purl.org/dc/elements/1.1/creator\n";
+$statement=RDF::Statement->new_from_nodes(undef, RDF::Node->new_from_uri_string("http://purl.org/dc/elements/1.1/creator"), undef);
 my $stream=$model->find_statements($statement);
 while(!$stream->end) {
-  $statement=$stream->next;
-  print "Statement: ",$statement->as_string,"\n";
-  my $subject=$statement->subject;
-  print "Subject: ",$subject->as_string,"\n";
-  print "Predicate: ",$statement->predicate->as_string,"\n";
-  print "Object: ",$statement->object->as_string,"\n";
+  my $statement2=$stream->next;
+  print "Matching Statement: ",$statement2->as_string,"\n";
+  my $subject=$statement2->subject;
+  print "  Subject: ",$subject->as_string,"\n";
+  print "  Predicate: ",$statement2->predicate->as_string,"\n";
+  print "  Object: ",$statement2->object->as_string,"\n";
 }
+$stream=undef;
