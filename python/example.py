3ab723a110c9d23b7efd5b962aec39abe98a5f21
Dave Beckett <dave@dajobe.org>
2001-02-04 22:01:56 +0000

    Python 2.0 example

---
+++ python/example.py
@@ -0,0 +1,65 @@
+#
+# example.py - Redland Python 2.0 example code
+#
+# $Id$
+#
+# Copyright (C) 2000-2001 David Beckett - http://purl.org/net/dajobe/
+# Institute for Learning and Research Technology - http://www.ilrt.org/
+# University of Bristol - http://www.bristol.ac.uk/
+# 
+# This package is Free Software or Open Source available under the
+# following licenses (these are alternatives):
+#   1. GNU Lesser General Public License (LGPL) Version 2
+#   2. GNU General Public License (GPL) Version 2
+#   3. Mozilla Public License (MPL) Version 1.1
+# and no other versions of those licenses.
+# 
+# See INSTALL.html or INSTALL.txt at the top of this package for the
+# full license terms.
+# 
+#
+
+import RDF
+
+world=RDF.world()
+
+storage=RDF.storage({'storage_name' : "hashes", 'name' : "test", "options_string" : "new='yes',hash-type='bdb',dir='.'"})
+if not storage:
+  raise "new RDF.storage failed"
+
+#world.debug(1)
+
+model=RDF.model(storage)
+if not model:
+  raise "new RDF.model failed"
+
+statement=RDF.statement({'subject' : RDF.node({"uri_string" : "http://purl.org/net/dajobe/"}),
+                        'predicate' : RDF.node({"uri_string" : "http://purl.org/dc/elements/1.1/creator"}),
+                        'object' : RDF.node({"literal" : "Dave Beckett"})})
+if not statement:
+  raise "new RDF.statement failed"
+
+# after this statement is owned by model and should not be used
+model.add_statement(statement)
+del statement
+
+# Match against an empty statement - find everything
+statement=RDF.statement({"subject" : None, "predicate" : None, "object": None});
+# after this statement should not be touched since find_statements is using it
+stream=model.find_statements(statement);
+
+while not stream.end():
+  statement2=stream.next();
+  print "found statement:",statement2
+del statement2
+
+del statement
+
+del stream
+
+del model
+del storage
+
+# this must be done last
+world.close()
+
