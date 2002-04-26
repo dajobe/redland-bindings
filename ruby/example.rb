b63108b80fc67b75408a314247c131f3022f7f7a
Dave Beckett <dave@dajobe.org>
2002-04-26 13:45:12 +0000

    Added header. Fixed typo

--- ruby/example.rb
+++ ruby/example.rb
@@ -1,4 +1,24 @@
 #!/usr/bin/env ruby
+#
+# example.rb - Redland example Ruby program
+#
+# $Id$
+#
+# Copyright (C) 2002 David Beckett - http://purl.org/net/dajobe/
+# Institute for Learning and Research Technology - http://www.ilrt.org/
+# University of Bristol - http://www.bristol.ac.uk/
+# 
+# This package is Free Software or Open Source available under the
+# following licenses (these are alternatives):
+#   1. GNU Lesser General Public License (LGPL)
+#   2. GNU General Public License (GPL)
+#   3. Mozilla Public License (MPL)
+# 
+# See LICENSE.html or LICENSE.txt at the top of this package for the
+# full license terms.
+# 
+# 
+#
 
 require 'Redland'
 
@@ -42,7 +62,7 @@ end
 
 Redland::librdf_free_stream stream
 
-puts "Parsing added count statements"
+puts "Parsing added #{count} statements"
 
 Redland::librdf_free_parser parser
 
