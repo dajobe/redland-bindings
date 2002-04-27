72b39a068218e5027ed44249775b0c2efe94c71c
Dave Beckett <dave@dajobe.org>
2002-04-27 23:20:00 +0000

    Expanded to print out model, parse into it, query model by statements and nodes and generally exercise most of the API techniques

--- python/test/test.py
+++ python/test/test.py
@@ -41,6 +41,7 @@ if not statement:
 
 model.add_statement(statement)
 
+print "printing all model statements"
 # Match against an empty statement - find everything
 statement=RDF.statement(subject=None, predicate=None, object=None);
 # after this statement should not be touched since find_statements is using it
@@ -48,16 +49,64 @@ stream=model.find_statements(statement);
 
 while not stream.end():
   statement2=stream.next();
-  print "found statement:",statement2
+  print "  found statement:",statement2
 del statement2
 
 del statement
 
 del stream
 
+
+parser=RDF.parser("raptor", "application/rdf+xml")
+if parser is None:
+  raise "Could not find any rdf/xml parser"
+
+uri=RDF.uri(string="file:../perl/dc.rdf")
+print "made uri", uri
+
+stream=parser.parse_as_stream(uri, uri)
+
+while not stream.end():
+  statement2=stream.next();
+  print "found parsed statement:",statement2
+  model.add_statement(statement2)
+stream=None
+
+# add it again just to get some more statements
+parser.parse_into_model(uri, uri, model)
+
+del uri
+del parser
+
+
+
+print "printing model"
+stream=model.serialise()
+
+while not stream.end():
+  statement2=stream.next();
+  print "found statement:",statement2
+stream=None
+
+print "searching model by statement"
+search_statement=RDF.statement(subject=None, predicate=RDF.node(uri_string="http://purl.org/dc/elements/1.1/title"), object=None)
+
+
+stream=model.find_statements(statement=search_statement)
+while not stream.end():
+  statement2=stream.next();
+  print "  found statement:",statement2
+stream=None
+
+print "searching model for node targets"
+n1=RDF.node(uri_string="http://purl.org/net/dajobe/")
+n2=RDF.node(uri_string="http://purl.org/dc/elements/1.1/title")
+for node in model.targets(n1,n2):
+  print "  found node:",node
+
+
 del model
 del storage
 
 # this must be done last
 world.close()
-
