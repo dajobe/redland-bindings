4fb6fb2fddd59df20fbc8e298bfddff9d690f59c
Dave Beckett <dave@dajobe.org>
2004-06-17 15:43:48 +0000

    Redland.dll Assembly info

---
+++ csharp/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+//
+// AssemblyInfo.cs - C# assembly information
+//
+// $Id$
+//
+// Copyright (C) 2004 David Beckett - http://purl.org/net/dajobe/
+// Institute for Learning and Research Technology - http://www.ilrt.bris.ac.uk/
+// University of Bristol - http://www.bristol.ac.uk/
+// 
+// This package is Free Software or Open Source available under the
+// following licenses (these are alternatives):
+//   1. GNU Lesser General Public License (LGPL)
+//   2. GNU General Public License (GPL)
+//   3. Mozilla Public License (MPL)
+// 
+// See LICENSE.html or LICENSE.txt at the top of this package for the
+// full license terms.
+// 
+// 
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// [assembly: CLSCompliant(true)]
+
+// General Assembly information
+[assembly: AssemblyTitle("Redland.dll")]
+[assembly: AssemblyDescription("Redland RDF Library")]
+[assembly: AssemblyConfiguration("Development version")]
+[assembly: AssemblyCompany("Redland developers")]
+[assembly: AssemblyProduct("Redland CLI")]
+[assembly: AssemblyCopyright("Copyright (C) 2004 Dave Beckett, Cesar Lopez Nataren")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+
+// Assembly versions have following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all values by your own or you can build default build and revision
+// numbers with the '*' character (the default):
+[assembly: AssemblyVersion("0.9.17.0")]
+
+// For assembly signing
+[assembly: AssemblyDelaySign(true)]
+[assembly: AssemblyKeyFile("redland.pub")]
+//[assembly: AssemblyKeyName("")]
