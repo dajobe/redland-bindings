5838642a5dee90b74f040be23d8899912eb08144
Dave Beckett <dave@dajobe.org>
2004-06-17 15:05:45 +0000

    Renamed util.cs to Util.cs

---
+++ csharp/Util.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Rdf {
+
+	public class Util {
+
+		[DllImport ("libc", EntryPoint="fopen")]
+		public static extern IntPtr fopen (string path, string mode);
+
+		[DllImport ("libc", EntryPoint="fputs")]
+		public static extern IntPtr fputs (string path, IntPtr fh);
+	}
+}
