e8fe02091f419b53beee5830197faa45c6e19cdc
Dave Beckett <dave@dajobe.org>
2004-07-08 22:42:19 +0000

    Added Errors, Warnings support (ClearLog): Added to empty the errors and warnings lists. (Enter): To lock world for gathering errors (Exit): To unlock world after gathering errors (setupHandlers): Use a delegate intErrorHandler (function pointer in C) to register callbacks for errors and warnings. (dispatchError, dispatchWarning): Added the error, warning gatherers.

--- csharp/World.cs
+++ csharp/World.cs
@@ -3,12 +3,15 @@
 //
 // Author:
 //	Cesar Lopez Nataren (cesar@ciencias.unam.mx)
+//	Edd Dumbill (edd@usefulinc.com)
 //
 // (C) 2004, Cesar Lopez Nataren
+// Edd Dumbill
 //
 
-using System;
-using System.Runtime.InteropServices;
+using System; using System.Runtime.InteropServices;
+using System.Threading;
+using System.Collections;
 
 namespace Redland {
 
@@ -16,12 +19,34 @@ namespace Redland {
 
 		IntPtr world;
 
+		internal ArrayList errors;
+		internal ArrayList warnings;
+
+		public string [] Errors {
+			get {
+				return (string []) errors.ToArray (typeof (String));
+			}
+		}
+		public string [] Warnings {
+			get { return (string []) warnings.ToArray (typeof (String)); }
+		}
+
+		public bool ErrorsWaiting {
+			get { return errors.Count > 0; }
+		}
+
+		public bool WarningsWaiting {
+			get { return warnings.Count > 0; }
+		}
+
 		[DllImport ("librdf")]
 		static extern IntPtr librdf_new_world ();
 
 		internal World ()
 		{
 			world = librdf_new_world ();
+			ClearLog ();
+			setupHandlers ();
 		}
 
 		[DllImport ("librdf")]
@@ -44,5 +69,52 @@ namespace Redland {
 			librdf_free_world (world);
 		}
 
+		public void ClearLog ()
+		{
+			errors = new ArrayList ();
+			warnings = new ArrayList ();
+		}
+
+		// called by anything that can get errors, in order to lock
+		// the world and clear the error logs
+		public void Enter ()
+		{
+			Monitor.Enter (this);
+			ClearLog ();
+		}
+
+		// called by anything that can get errors, in order to unlock
+		// the world
+		public void Exit ()
+		{
+			Monitor.Exit (this);
+		}
+
+		private delegate int intErrorHandler (IntPtr userdata, IntPtr message, IntPtr args);
+
+		[DllImport ("librdf")]
+		static extern void librdf_world_set_error (IntPtr world, IntPtr userdata, Delegate cb);
+		[DllImport ("librdf")]
+		static extern void librdf_world_set_warning (IntPtr world, IntPtr userdata, Delegate cb);
+
+		private int dispatchError (IntPtr userdata, IntPtr message, IntPtr args)
+		{
+			errors.Add (Marshal.PtrToStringAuto (message));
+			return 1;
+		}
+
+		private int dispatchWarning (IntPtr userdata, IntPtr message, IntPtr args)
+		{
+			warnings.Add (Marshal.PtrToStringAuto (message));
+			return 1;
+		}
+
+		private void setupHandlers ()
+		{
+			librdf_world_set_error (world,
+					new IntPtr (0), new intErrorHandler (dispatchError));
+			librdf_world_set_warning (world,
+					new IntPtr (0), new intErrorHandler (dispatchWarning));
+		}
 	}
 }
