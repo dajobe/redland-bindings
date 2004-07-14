ec20428ddf51c7acc2f50ad8ab6b58cf4fc4cf47
Dave Beckett <dave@dajobe.org>
2004-07-14 13:33:26 +0000

    Edd Dumbill <edd@usefulinc.com> general indenting fixes. Implement IDisposable. Parser: init to IntPtr.Zero (Parser (World, string, string, Uri)): remove ability to construct with null World. (ParseIntoModel): add FIXME note. (ParseAsStream): add FIXME note. (ParseStringAsStream): add FIXME note.

--- csharp/Parser.cs
+++ csharp/Parser.cs
@@ -18,9 +18,11 @@ namespace Redland {
 			base (msg, errs) { }
 	}
 
-	public class Parser {
+	public class Parser : IWrapper, IDisposable {
 
-		IntPtr parser;
+		IntPtr parser = IntPtr.Zero;
+
+		bool disposed = false;
 
 		public IntPtr Handle {
 			get { return parser; }
@@ -48,12 +50,7 @@ namespace Redland {
 		{
 			IntPtr iname = Marshal.StringToHGlobalAuto (name);
 			IntPtr imime_type = Marshal.StringToHGlobalAuto (mime_type);
-			if (world == null)
-				if ((Object)uri == null)
-					parser = librdf_new_parser (IntPtr.Zero, iname, imime_type, IntPtr.Zero);
-				else
-					parser = librdf_new_parser (IntPtr.Zero, iname, imime_type, uri.Handle);
-			else if ((Object)uri == null)
+			if (uri == (Uri) null)
 				parser = librdf_new_parser (world.Handle, iname, imime_type, IntPtr.Zero);
 			else
 				parser = librdf_new_parser (world.Handle, iname, imime_type, uri.Handle);
@@ -66,20 +63,41 @@ namespace Redland {
 		[DllImport ("librdf")]
 		static extern void librdf_free_parser (IntPtr parser);
 
+		protected void Dispose (bool disposing)
+		{
+			if (! disposed) {
+				// if disposing is true, then dispose of
+				// managed resources
+
+				if (parser != IntPtr.Zero) {
+					librdf_free_parser (parser);
+					parser = IntPtr.Zero;
+				}
+
+				disposed = true;
+			}
+		}
+
+		public void Dispose ()
+		{
+			Dispose (true);
+			GC.SuppressFinalize (this);
+		}
+
 		~Parser ()
 		{
-			if(parser != (IntPtr)null)
-				librdf_free_parser (parser);
+			Dispose (false);
 		}
 
 
 		[DllImport ("librdf")]
 		static extern int librdf_parser_parse_string_into_model (IntPtr parser, IntPtr s, IntPtr base_uri, IntPtr model);
+
 		public int ParseStringIntoModel (string s, Uri base_uri, Model model)
 		{
 			IntPtr istr = Marshal.StringToHGlobalAuto (s);
 			Redland.World.Enter ();
-			int rc=librdf_parser_parse_string_into_model (Handle, istr, base_uri.Handle, model.Handle);
+			int rc = librdf_parser_parse_string_into_model (Handle, istr, base_uri.Handle, model.Handle);
 			Marshal.FreeHGlobal (istr);
 			LogMessage [] errs = Redland.World.Messages;
 			Redland.World.Exit ();
@@ -94,11 +112,12 @@ namespace Redland {
 		public int ParseIntoModel (Uri uri, Uri base_uri, Model model)
 		{
 			Redland.World.Enter ();
-			int rc=librdf_parser_parse_into_model (parser, uri.Handle, base_uri.Handle, model.Handle);
+			int rc = librdf_parser_parse_into_model (parser, uri.Handle, base_uri.Handle, model.Handle);
 			LogMessage [] errs = Redland.World.Messages;
 			Redland.World.Exit ();
 			if (errs.Length > 0)
 				throw new ParseError ("Parsing error", errs);
+			// FIXME: can we remove rc now we throw exceptions?
 			return rc;
 		}
 
@@ -115,6 +134,7 @@ namespace Redland {
 			Uri tmp_uri = new Redland.Uri (uri);
 			Redland.World.Enter ();
 			IntPtr raw_stream = librdf_parser_parse_as_stream (parser, tmp_uri.Handle, IntPtr.Zero);
+			// FIXME: throw exception if raw_stream is zero ?
 			LogMessage [] errs = Redland.World.Messages;
 			Redland.World.Exit ();
 			if (errs.Length > 0)
@@ -133,6 +153,8 @@ namespace Redland {
 			IntPtr istr = Marshal.StringToHGlobalAuto (s);
 			Redland.World.Enter ();
 			IntPtr raw_ret = librdf_parser_parse_string_as_stream (parser, istr, base_uri.Handle);
+			// FIXME: throw exception if raw_ret is zero ? currently
+			// we return null, see below.
 			Stream stream;
 			Marshal.FreeHGlobal (istr);
 			LogMessage [] errs = Redland.World.Messages;
