3c2e52e4166f044c806906bc3ee0d0ac8bed3086
Dave Beckett <dave@dajobe.org>
2004-07-14 13:34:10 +0000

    Edd Dumbill <edd@usefulinc.com> general indenting fixes. Implement IDisposable. query: init to IntPtr.Zero. (Query (Word, string, Object, string)): init iuri to IntPtr.Zero, not null.  Change third parameter to Uri type, not Object. (Query (string)): use constified name for RDQL. (Execute): adjust to new form of QueryResults ctor.

--- csharp/Query.cs
+++ csharp/Query.cs
@@ -13,43 +13,65 @@ using System.Runtime.InteropServices;
 
 namespace Redland {
 
-	public class Query : IWrapper {
+	public class Query : IWrapper, IDisposable {
 		
-		IntPtr query;
+		IntPtr query = IntPtr.Zero;
+		bool disposed = false;
+
+		public static string RDQL = "rdql";
 
 		public IntPtr Handle {
 			get { return query; }
 		}
 
 		public Query (string s)
-			: this (Redland.World, s, null, "rdql")
+			: this (Redland.World, s, null, RDQL)
 		{
 		}
 
 		[DllImport ("librdf")]
 		static extern IntPtr librdf_new_query (IntPtr world, IntPtr query_language, IntPtr uri, IntPtr query_string);
 
-		private Query (World world, string s, Object uri, string query_language)
+		private Query (World world, string s, Uri uri, string query_language)
 		{
 			IntPtr iql = Marshal.StringToHGlobalAuto (query_language.ToString());
 			IntPtr iqs = Marshal.StringToHGlobalAuto (s.ToString());
-                        IntPtr iuri=(IntPtr)null;
+			IntPtr iuri = IntPtr.Zero;
                         
-                        if(uri is Uri)
-                          iuri=((Uri)uri).Handle;
+			if (uri != (Uri) null)
+				iuri = uri.Handle;
                         
 			query = librdf_new_query (Redland.World.Handle, iql, iuri, iqs);
-                        Marshal.FreeHGlobal (iql);
-                        Marshal.FreeHGlobal (iqs);
+			Marshal.FreeHGlobal (iql);
+			Marshal.FreeHGlobal (iqs);
 		}
 
 		[DllImport ("librdf")]
 		static extern void librdf_free_query (IntPtr query);
 
+		protected void Dispose (bool disposing)
+		{
+			if (! disposed) {
+				// if disposing is true, dispose of managed
+				// resources
+
+				if (query != IntPtr.Zero) {
+					librdf_free_query (query);
+					query = IntPtr.Zero;
+				}
+				disposed = true;
+			}
+		}
+
+		public void Dispose ()
+		{
+			Dispose (true);
+			GC.SuppressFinalize (this);
+		}
+
 		~Query ()
 		{
-			if(query != (IntPtr)null)
-				librdf_free_query (query);
+			Dispose (false);
 		}
 
 		[DllImport ("librdf")]
@@ -58,10 +80,9 @@ namespace Redland {
 		public QueryResults Execute (Model model)
 		{
 			IntPtr raw_qr = librdf_query_execute (query, model.Handle);
-			QueryResults qr = new QueryResults (this,raw_qr);
+			// FIXME: throw exception if raw_qr is zero
+			QueryResults qr = new QueryResults (raw_qr);
 			return qr;
 		}
-
-
 	}
 }
