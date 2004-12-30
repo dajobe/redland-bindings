de5a8cd5bd10295a3269d0e3b72f49e9816e0b0e
Dave Beckett <dave@dajobe.org>
2004-12-30 21:42:56 +0000

    Update from http://lists.w3.org/Archives/Public/www-archive/2004Dec/att-0053/foaf.rb by danbri

--- ruby/lib/rdf/redland/schemas/foaf.rb
+++ ruby/lib/rdf/redland/schemas/foaf.rb
@@ -1,17 +1,12 @@
-require 'rdf/redland'
-require 'rdf/redland/resource'
-require 'rdf/redland/constants'
+require 'rdf'
 
-
-module Redland::FOAF
-  include Redland
+module FOAF
 
   NS = Redland::Namespace.new('http://xmlns.com/foaf/0.1/')
   FIRST = NS['first']
   SURNAME = NS['surname']
   PHONE = NS['phone']
   TITLE = NS['title']
-  #  NAME = NS['name']
   MBOX = NS['mbox']
   MBOXSHAL1 = NS['mbox_shal1sum']
   NAME = NS['name']
@@ -21,69 +16,38 @@ module Redland::FOAF
   DEPICTS = NS['depicts']
   PERSON = NS['Person']
   KNOWS = NS['knows']
-
-  def first()
-    self.get_property(NS['firstName']).to_s
-  end
-
-  def first=(first,context=@context)
-    self.add_property(NS['firstName'],first,context)
-  end
-
-  def name()
-    self.get_property(NS['name']).to_s
-  end
-
-  def name=(name,context=@context)
-    self.add_property(NS['name'],name,context)
-  end
-
-  def title()
-    self.get_property(NS['title']).to_s
-  end
-
-  def title=(title,context=@context)
-    self.add_property(NS['title'],title,context)
-  end
-
-  def surname()
-    self.get_property(NS['surname']).to_s
-  end
-
-  def surname=(surname,context=@context)
-    self.add_property(NS['surname'],surname,context)
-  end
-
-  def phone()
-    self.get_property(NS['phone']).to_s
-  end
-
-  def phone=(phone,context=@context)
-    self.add_property(NS['phone'],phone,context)
-  end
   
-  def mbox()
-    self.get_property(MBOX).to_s
-  end 
 
-  def mbox=(mbox,context=@context)
-    self.add_property(NS['mbox'],mbox,context)
-    mboxshal1()
+  def foaf_first(node)
+    @model.object(node,FOAF['firstName']).to_s
   end
 
-  def mboxshal1()
-    mboxshal = self.get_property(MBOXSHAL1)
-    unless mboxshal
-      mboxshal1 = "Generated from #{mbox}"
-      self.add_property(MBOXSHAL1,mboxshal1,Resource.new('http://generated/'))
-    end
+  def foaf_name(node)
+    @model.object(node,NAME)
   end
 
-  def to_Person(context=@context)
-    self.set_type(NS['Person'],context)
+  def foaf_title(node)
+    @model.object(node,FOAF['title']).to_s
   end
 
-  #self.add_property(TYPE,NS['Person'])
-  
+  def foaf_surname(node)
+    @model.object(node,FOAF['surname']).to_s
+  end
+
+  def foaf_phone(node)
+    @model.object(node,FOAF['phone']).to_s
+  end
+
+  def foaf_namespace
+    return FOAF
+  end
+
+  def foaf_mbox(node)
+    @model.object(node,MBOX).to_s
+  end
+
+  def foaf_name(node)
+    @model.object(node,NAME).to_s
+  end
 
 end
