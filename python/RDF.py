9f8c6a32c2f70285fb244bbb3b1633ad1e8666ab
Dave Beckett <dave@dajobe.org>
2005-01-18 08:18:38 +0000

    Update license to Apache style and for 2005

--- python/RDF.py
+++ python/RDF.py
@@ -3,21 +3,24 @@
 #
 # $Id$
 #
-# Copyright (C) 2000-2004 David Beckett - http://purl.org/net/dajobe/
+# Copyright (C) 2000-2005 David Beckett - http://purl.org/net/dajobe/
 # Institute for Learning and Research Technology - http://www.ilrt.bris.ac.uk/
 # University of Bristol - http://www.bristol.ac.uk/
 # 
-# This package is Free Software or Open Source available under the
-# following licenses (these are alternatives):
-#   1. GNU Lesser General Public License (LGPL)
-#   2. GNU General Public License (GPL)
-#   3. Mozilla Public License (MPL)
+# This package is Free Software and part of Redland http://librdf.org/
+# 
+# It is licensed under the following three licenses as alternatives:
+#   1. GNU Lesser General Public License (LGPL) V2.1 or any newer version
+#   2. GNU General Public License (GPL) V2 or any newer version
+#   3. Apache License, V2.0 or any newer version
 #
-# and as an exception this one file RDF.py is also available under the
-#   4. BSD License without advertising (MIT license - from
-#        http://www.opensource.org/licenses/mit-license.html)
+# This one file RDF.py is also available under the following license:
+#
+#   4. BSD License without advertising (aka MIT license)
+#      from http://www.opensource.org/licenses/mit-license.html
+#
 #      -------------------------------------------------------------------
-#      Copyright (c) 2003-2004, David Beckett, ILRT, University of Bristol
+#      Copyright (c) 2000-2005, David Beckett, ILRT, University of Bristol
 #
 #      Permission is hereby granted, free of charge, to any person
 #      obtaining a copy of this software and associated documentation
@@ -40,10 +43,12 @@
 #      SOFTWARE.
 #     --------------------------------------------------------------------
 #
+# You may not use this file except in compliance with at least one of
+# the above four licenses.
 # 
 # See LICENSE.html or LICENSE.txt at the top of this package for the
-# full license terms.
-# 
+# complete terms and further detail along with the license texts for
+# the licenses in COPYING.LIB, COPYING and LICENSE-2.0.txt respectively.
 # 
 #
 
