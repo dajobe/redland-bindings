93d840bed6e61b1297d79657a59f7116191eda93
Dave Beckett <dave@dajobe.org>
2005-04-08 13:38:51 +0000

    (set_log_handler, reset_log_handler): Now public, document and prefer them.

--- perl/lib/RDF/Redland.pm
+++ perl/lib/RDF/Redland.pm
@@ -96,24 +96,6 @@ $Warning_Sub=undef;
 
 $Log_Sub=undef;
 
-sub set_error_handler ($) {
-  $Error_Sub=shift;
-}
-
-sub set_warning_handler ($) {
-  $Warning_Sub=shift;
-}
-
-# Internal
-sub set_log_handler ($) {
-  $Log_Sub=shift;
-}
-
-sub reset_log_handler () {
-  $Log_Sub=undef;
-}
-
-
 =pod
 
 =head1 NAME
@@ -132,11 +114,67 @@ RDF::Redland - Redland RDF Class
 
 This class initialises the Redland RDF classes.
 
+See the main classes for full detail:
+L<RDF::Redland::Node>, 
+L<RDF::Redland::BlankNode>, L<RDF::Redland::URINode>,
+L<RDF::Redland::LiteralNode>, L<RDF::Redland::XMLLiteralNode>,
+L<RDF::Redland::URI>,
+L<RDF::Redland::Statement>, L<RDF::Redland::Model>,
+L<RDF::Redland::Storage>, L<RDF::Redland::Parser>,
+L<RDF::Redland::Query>, L<RDF::Redland::QueryResults>,
+L<RDF::Redland::Iterator>, L<RDF::Redland::Stream>
+and L<RDF::Redland::RSS>.
+
 =head1 STATIC METHODS
 
 =over
 
+=item set_log_handler SUB
+
+Set I<SUB> as the subroutine to be called on any Redland error, warning
+or log message.  The subroutine must have the followign signature:
+
+
+  sub handler ($$$$$$$$$) {
+    my($code, $level, $facility, $message, $line, $column, $byte, $file, $uri)=@_;
+    # int error code
+    # int log level
+    # int facility causing the error (parsing, serializing, ...)
+    # string error message
+    # int line number (<0 if not relevant)
+    # int column number (<0 if not relevant)
+    # int byte number (<0 if not relevant)
+    # string file name or undef
+    # string URI or undef
+    
+    # ...do something with the information ...
+  };
+
+  RDF::Redland::set_log_handler(\&handler);
+
+=cut
+
+sub set_log_handler ($) {
+  $Log_Sub=shift;
+}
+
+
+=item reset_log_handler
+
+Reset redland to use the default logging handler, typically printing
+the message to stdout or stderr and exiting on a fatal error.
+
+=cut
+
+sub reset_log_handler () {
+  $Log_Sub=undef;
+}
+
+
 =item set_error_handler SUB
+
+The method set_log_handler is much more flexible than this and includes
+this functionality.
 
 Set I<SUB> as the subroutine to be called on a Redland error with
 the error message as the single argument.  For example:
@@ -148,7 +186,17 @@ the error message as the single argument.  For example:
 
 The default if this is not set, is to run die $msg
 
+=cut
+
+sub set_error_handler ($) {
+  $Error_Sub=shift;
+}
+
+
 =item set_warning_handler SUB
+
+The method set_log_handler is much more flexible than this and includes
+this functionality.
 
 Set I<SUB> as the subroutine to be called on a Redland warning with
 the warning message as the single argument.  For example:
@@ -159,6 +207,14 @@ the warning message as the single argument.  For example:
   });
 
 The default if this is not set, is to run warn $msg
+
+=cut
+
+sub set_warning_handler ($) {
+  $Warning_Sub=shift;
+}
+
+=pod
 
 =back
 
