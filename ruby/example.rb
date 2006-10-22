7fc618b34315a35e9c3c0aba430e1370a476c168
Dave Beckett <dave@dajobe.org>
2006-10-22 05:23:03 +0000

    Added query and serializer test code

--- ruby/example.rb
+++ ruby/example.rb
@@ -62,3 +62,31 @@ while !stream.end?()
   puts "Statement: #{statement}"
   stream.next()
 end
+
+q = Redland::Query.new(" PREFIX dc: <http://purl.org/dc/elements/1.1/> SELECT ?a ?c WHERE { ?a dc:title ?c } ")
+puts "Querying for dc:titles:"
+results=q.execute(model)
+while !results.finished?()
+  puts "{"
+  for k in 0..results.bindings_count()-1
+    puts "  #{k}= #{results.binding_value(k)}"
+  end
+  puts "}"
+  results.next()
+end
+
+results=q.execute(model)
+size=results.to_string(Redland::Uri.new("http://www.w3.org/2001/sw/DataAccess/json-sparql/")).length()
+puts "Serialized query results to JSON as a string size #{size} bytes"
+
+print "Writing model to test-out.rdf as rdf/xml"
+# Use any rdf/xml parser that is available
+serializer=Redland::Serializer.new()
+serializer.set_namespace("dc", Redland::Uri.new("http://purl.org/dc/elements/1.1/"))
+serializer.set_namespace("rdf", Redland::Uri.new("http://www.w3.org/1999/02/22-rdf-syntax-ns#"))
+serializer.to_file("test-out.rdf", model)
+
+size=model.to_string(name="ntriples", base_uri=Redland::Uri.new("http://example.org/base#")).length()
+puts "Serialized to ntriples as a string size #{size} bytes"
+
+puts "Done"
