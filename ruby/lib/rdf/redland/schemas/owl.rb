0a19d3d845dcc0d80cd5c3058a06efa7a418b8c1
Dave Beckett <dave@dajobe.org>
2006-10-22 05:11:25 +0000

    Added OWLNS Fixes Issue#0000121 http://bugs.librdf.org/mantis/view.php?id=121

--- ruby/lib/rdf/redland/schemas/owl.rb
+++ ruby/lib/rdf/redland/schemas/owl.rb
@@ -4,6 +4,7 @@ module Redland::OWL
   #OWL
 
   OWL = Namespace.new("http://www.w3.org/2002/07/owl#")
+  OWLNS = Redland::Namespace.new("http://www.w3.org/2002/07/owl#")
   OWL_CLASS = OWLNS['Class']
   OWL_OBJECT_PROPERTY = OWLNS['ObjectProperty']
   OWL_DATATYPE_PROPERTY = OWLNS['DatatypeProperty']
