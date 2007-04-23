867794f95286e564add27d5a1c6901f3c6b5a532
Dave Beckett <dave@dajobe.org>
2007-04-23 03:22:05 +0000

    Apply patch from #0000121 again to fix issue http://bugs.librdf.org/mantis/view.php?id=121

--- ruby/lib/rdf/redland/schemas/owl.rb
+++ ruby/lib/rdf/redland/schemas/owl.rb
@@ -3,7 +3,6 @@ require 'rdf/redland'
 module Redland::OWL
   #OWL
 
-  OWL = Namespace.new("http://www.w3.org/2002/07/owl#")
   OWLNS = Redland::Namespace.new("http://www.w3.org/2002/07/owl#")
   OWL_CLASS = OWLNS['Class']
   OWL_OBJECT_PROPERTY = OWLNS['ObjectProperty']
