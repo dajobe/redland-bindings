9002fdde90554183237e7a881b1600630a386493
Dave Beckett <dave@dajobe.org>
2007-08-23 04:29:29 +0000

    GRDDL parsing with URI filtering

---
+++ python/filter.py
@@ -0,0 +1,23 @@
+# GRDDL parsing with URI filtering
+# This file is in the public domain
+
+import RDF
+
+source="http://www.w3.org/People/Connolly/"
+#source="http://www.w3.org/2001/sw/grddl-wg/td/titleauthor.html"
+
+model=RDF.Model()
+uri=RDF.Uri(string=source)
+parser=RDF.GRDDLParser()
+
+def reject_uri(uri):
+  print "********** reject_uri(",uri,")"
+  if "http://purl.org/NET/erdf/profile" == uri:
+    return True
+  return False
+
+parser.set_uri_filter(reject_uri)
+
+parser.parse_into_model(model, uri,uri)
+
+print "Parsing ",source,"added",model.size(),"statements"
