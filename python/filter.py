199f896edc91e1d39a0a2d9134a32639ce2641a1
Dave Beckett <dave@dajobe.org>
2007-12-25 02:46:37 +0000

    1.0.7.1

--- python/filter.py
+++ python/filter.py
@@ -19,5 +19,9 @@ def reject_uri(uri):
 parser.set_uri_filter(reject_uri)
 
 parser.parse_into_model(model, uri,uri)
+parser.set_feature(RDF.Uri("http://feature.librdf.org/raptor-htmlTagSoup"), "0")
+parser.set_feature(RDF.Uri("http://feature.librdf.org/raptor-microformats"), "0")
+parser.set_feature(RDF.Uri("http://feature.librdf.org/raptor-htmlLink"), "0")
+
 
 print "Parsing ",source,"added",model.size(),"statements"
