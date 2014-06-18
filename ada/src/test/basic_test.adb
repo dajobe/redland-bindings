94454ed6daab5d07930e8e1167824ddff0574c0d
Victor Porton <porton.victor@gmail.com>
2014-06-19 00:37:45 +0300

    Cleaned the testing code

--- ada/src/test/basic_test.adb
+++ ada/src/test/basic_test.adb
@@ -1,17 +1,16 @@
+with AUnit.Test_Cases;
 with RDF.Raptor;
 use all type RDF.Raptor.Flag_Type;
 
--- This just creates and assigns some variables (and ignores their values)
-
 package body Basic_Test is
 
-   Default_World: RDF.Raptor.World := RDF.Raptor.Open;
-
-   World_With_Some_Flags: RDF.Raptor.World := RDF.Raptor.Open((1=>(Flag=>URI_Interning, Value=>False)));
-
-   ----------
-   -- Name --
-   ----------
+   -- This just creates and assigns some variables (and ignores their values)
+   procedure Test_Open(T : in out Test_Cases.Test_Case'Class) is
+      Default_World: RDF.Raptor.World := RDF.Raptor.Open;
+      World_With_Some_Flags: RDF.Raptor.World := RDF.Raptor.Open((1=>(Flag=>URI_Interning, Value=>False)));
+   begin
+      null;
+   end;
 
    function Name (T : Test_Case)
                   return Test_String is
@@ -19,10 +18,10 @@ package body Basic_Test is
       return Format ("Constructors of RDF worlds");
    end Name;
 
-   -- The test consists solely of creating variables, no test procedures are run.
    procedure Register_Tests (T : in out Test_Case) is
+      use AUnit.Test_Cases.Registration;
    begin
-      null;
+      Register_Routine (T, Test_Open'Access, "Testing the constructors and function Open");
    end Register_Tests;
 
 end Basic_Test;
