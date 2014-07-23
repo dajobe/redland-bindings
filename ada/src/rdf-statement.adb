64dc27fc5be3761e74963622874d2365ce690d9e
Victor Porton <porton.victor@gmail.com>
2014-07-23 19:12:32 +0300

    small changes

--- ada/src/rdf-statement.adb
+++
@@ -1,9 +0,0 @@
-package body RDF.Statement is
-
-   procedure Handle (Handler: Statement_Handler) is abstract;
-
-   procedure C_Statement_Handler (User_Data: Dummy_Record_Access; ...) is
-   begin
-
-
-end RDF.Statement;
--- ada/src/rdf-statement.ads
+++ ada/src/rdf-statement.ads
@@ -1,7 +1,5 @@
 package RDF.Statement is
 
-   pragma Elaborate_Body; -- FIXME: Remove
-
 --     type Statement_Handler is abstract tagged null record;
 --
 --     procedure Handle (Handler: Statement_Handler) is abstract;
