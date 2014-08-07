d5642bb9079eba58085394417e6fcdb9b1b61476
Victor Porton <porton.victor@gmail.com>
2014-08-07 20:12:11 +0300

    more comments about memory leak

--- ada/src/test/special_test.adb
+++ ada/src/test/special_test.adb
@@ -1,7 +1,7 @@
--- FIXME: Calling this as
+-- FIXME: Calling this (compiled with GANT 4.9) as
 --   ltrace -n4 -llibraptor2.so.0 ./obj/test/check/special_test 2>&1| egrep ^[a-z]
 -- shows that there are more raptor_new_uri_from_counted_string() + raptor_uri_copy()
--- than raptor_free_uri().
+-- calls than raptor_free_uri() calls.
 -- This means memory leak.
 
 with Ada.Finalization;
