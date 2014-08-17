e161c5000d114b43e34b1ac63700d7fafdb5c1c9
Victor Porton <porton.victor@gmail.com>
2014-08-18 01:34:18 +0300

    soem work about options

--- ada/src/rdf-raptor-options.ads
+++ ada/src/rdf-raptor-options.ads
@@ -1,5 +1,7 @@
 with Interfaces.C; use Interfaces.C;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
 with RDF.Auxilary.Limited_Handled_Record;
+with RDF.Raptor.URI; use RDF.Raptor.URI;
 
 package RDF.Raptor.Options is
 
@@ -45,66 +47,93 @@ package RDF.Raptor.Options is
                           WWW_SSL_VERIFY_PEER,
                           WWW_SSL_VERIFY_HOST,
                           LOAD_EXTERNAL_ENTITIES)
-      with Convention => C;
-   for Raptor_Option use (SCANNING => 0,
-                          ALLOW_NON_NS_ATTRIBUTES => 1,
-                          ALLOW_OTHER_PARSETYPES => 2,
-                          ALLOW_BAGID => 3,
-                          ALLOW_RDF_TYPE_RDF_LIST => 4,
-                          NORMALIZE_LANGUAGE => 5,
-                          NON_NFC_FATAL => 6,
-                          WARN_OTHER_PARSETYPES => 7,
-                          CHECK_RDF_ID => 8,
-                          RELATIVE_URIS => 9,
-                          WRITER_AUTO_INDENT => 10,
-                          WRITER_AUTO_EMPTY => 11,
-                          WRITER_INDENT_WIDTH => 12,
-                          WRITER_XML_VERSION => 13,
-                          WRITER_XML_DECLARATION => 14,
-                          NO_NET => 15,
-                          RESOURCE_BORDER => 16,
-                          LITERAL_BORDER => 17,
-                          BNODE_BORDER => 18,
-                          RESOURCE_FILL => 19,
-                          LITERAL_FILL => 20,
-                          BNODE_FILL => 21,
-                          HTML_TAG_SOUP => 22,
-                          MICROFORMATS => 23,
-                          HTML_LINK => 24,
-                          WWW_TIMEOUT => 25,
-                          WRITE_BASE_URI => 26,
-                          WWW_HTTP_CACHE_CONTROL => 27,
-                          WWW_HTTP_USER_AGENT => 28,
-                          JSON_CALLBACK => 29,
-                          JSON_EXTRA_DATA => 30,
-                          RSS_TRIPLES => 31,
-                          ATOM_ENTRY_URI => 32,
-                          PREFIX_ELEMENTS => 33,
-                          STRICT => 34,
-                          WWW_CERT_FILENAME => 35,
-                          WWW_CERT_TYPE => 36,
-                          WWW_CERT_PASSPHRASE => 37,
-                          NO_FILE => 38,
-                          WWW_SSL_VERIFY_PEER => 39,
-                          WWW_SSL_VERIFY_HOST => 40,
-                          LOAD_EXTERNAL_ENTITIES => 41);
+     with Convention => C;
+   -- Use the default representation (it may probably be faster)
+--     for Raptor_Option use (SCANNING => 0,
+--                            ALLOW_NON_NS_ATTRIBUTES => 1,
+--                            ALLOW_OTHER_PARSETYPES => 2,
+--                            ALLOW_BAGID => 3,
+--                            ALLOW_RDF_TYPE_RDF_LIST => 4,
+--                            NORMALIZE_LANGUAGE => 5,
+--                            NON_NFC_FATAL => 6,
+--                            WARN_OTHER_PARSETYPES => 7,
+--                            CHECK_RDF_ID => 8,
+--                            RELATIVE_URIS => 9,
+--                            WRITER_AUTO_INDENT => 10,
+--                            WRITER_AUTO_EMPTY => 11,
+--                            WRITER_INDENT_WIDTH => 12,
+--                            WRITER_XML_VERSION => 13,
+--                            WRITER_XML_DECLARATION => 14,
+--                            NO_NET => 15,
+--                            RESOURCE_BORDER => 16,
+--                            LITERAL_BORDER => 17,
+--                            BNODE_BORDER => 18,
+--                            RESOURCE_FILL => 19,
+--                            LITERAL_FILL => 20,
+--                            BNODE_FILL => 21,
+--                            HTML_TAG_SOUP => 22,
+--                            MICROFORMATS => 23,
+--                            HTML_LINK => 24,
+--                            WWW_TIMEOUT => 25,
+--                            WRITE_BASE_URI => 26,
+--                            WWW_HTTP_CACHE_CONTROL => 27,
+--                            WWW_HTTP_USER_AGENT => 28,
+--                            JSON_CALLBACK => 29,
+--                            JSON_EXTRA_DATA => 30,
+--                            RSS_TRIPLES => 31,
+--                            ATOM_ENTRY_URI => 32,
+--                            PREFIX_ELEMENTS => 33,
+--                            STRICT => 34,
+--                            WWW_CERT_FILENAME => 35,
+--                            WWW_CERT_TYPE => 36,
+--                            WWW_CERT_PASSPHRASE => 37,
+--                            NO_FILE => 38,
+--                            WWW_SSL_VERIFY_PEER => 39,
+--                            WWW_SSL_VERIFY_HOST => 40,
+--                            LOAD_EXTERNAL_ENTITIES => 41);
 
    function Last return Raptor_Option renames LOAD_EXTERNAL_ENTITIES;
 
-   type Option_Description_Type is private;
+   type Value_Type_Type is (TYPE_BOOL, TYPE_INT, TYPE_STRING, TYPE_URI)
+     with Convention => C;
 
-   package Handled_Record is new RDF.Auxilary.Limited_Handled_Record(Option_Description_Type);
+   function Last return Value_Type_Type renames TYPE_URI;
+
+   type Option_Description_Record is private;
+
+   package Handled_Record is new RDF.Auxilary.Limited_Handled_Record(Option_Description_Record);
 
    type Option_Description_Type_Without_Finalize is new Handled_Record.Base_Object with null record;
 
    subtype Handle_Type is Handled_Record.Access_Type;
 
+   function Get_Domain (Description: Option_Description_Type_Without_Finalize) return Domain_Type;
+
+   function Get_Option (Description: Option_Description_Type_Without_Finalize) return Raptor_Option;
+
+   function Get_Value_Type (Description: Option_Description_Type_Without_Finalize) return Value_Type_Type;
+
+   function Get_Name (Description: Option_Description_Type_Without_Finalize) return String;
+
+   function Get_Label (Description: Option_Description_Type_Without_Finalize) return String;
+
+   function Get_URI (Description: Option_Description_Type_Without_Finalize) return URI_Type_Without_Finalize;
+
+   -- TODO: Bodies for the above functions.
+
+   -- TODO: Stopped at raptor_option_get_count ()
+
 private
 
-   type Option_Description_Type is
+   type Option_Description_Record is
       record
          Domain: Domain_Type;
-         -- TODO
+         Option: Raptor_Option;
+         Value_Type: Value_Type_Type;
+         Name: chars_ptr;
+         Len: size_t;
+         Label: chars_ptr;
+         URI: RDF.Raptor.URI.Handle_Type;
       end record
       with Convention => C;
 
