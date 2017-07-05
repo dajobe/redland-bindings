60cdb01f15d21dbecfb21a0b217c2f8b74809a15
Victor Porton <porton@narod.ru>
2017-07-06 00:56:17 +0300

    some subprograms implemented

--- ada/src/rdf-raptor-www.adb
+++ ada/src/rdf-raptor-www.adb
@@ -9,4 +9,21 @@ package body RDF.Raptor.WWW is
       return From_Non_Null_Handle(C_Raptor_New_WWW(Get_Handle(World)));
    end;
 
+   function C_Raptor_New_WWW_With_Connection (World: RDF.Raptor.World.Handle_Type; Connection: Connection_Type) return WWW_Handle_Type
+      with Import, Convention=>C, External_Name=>"raptor_new_www_with_connection";
+
+   function New_WWW (World: RDF.Raptor.World.World_Type'Class; Connection: Connection_Type) return WWW_Type is
+      use RDF.Raptor.World;
+   begin
+      return From_Non_Null_Handle(C_Raptor_New_WWW_With_Connection(Get_Handle(World), Connection));
+   end;
+
+   procedure C_Raptor_Free_WWW (Handle: WWW_Handle_Type)
+      with Import, Convention=>C, External_Name=>"raptor_free_www";
+
+   procedure Finalize_Handle (Object: WWW_Type; Handle: WWW_Handle_Type) is
+   begin
+      C_Raptor_Free_WWW(Handle);
+   end;
+
 end RDF.Raptor.WWW;
--- ada/src/rdf-raptor-www.ads
+++ ada/src/rdf-raptor-www.ads
@@ -7,14 +7,22 @@ package RDF.Raptor.WWW is
 
    subtype WWW_Handle_Type is WWW_Handled_Record.Access_Type;
 
+   -- I deliberately expose that it is an access type (not just a private type),
+   -- to simplify libcurl and libxml interaction
+   type Connection_Type is new RDF.Auxiliary.Dummy_Record_Access;
+
    type WWW_Type_Without_Finalize is new WWW_Handled_Record.Base_Object with null record;
 
 
 
    type WWW_Type is new WWW_Type_Without_Finalize with null record;
 
-   function New_WWW (World: RDF.Raptor.World.World_Type'Class) return WWW_Type;
+   not overriding function New_WWW (World: RDF.Raptor.World.World_Type'Class) return WWW_Type;
 
-   -- TODO: Stopped at raptor_new_www_with_connection()
+   not overriding function New_WWW (World: RDF.Raptor.World.World_Type'Class; Connection: Connection_Type) return WWW_Type;
+
+   overriding procedure Finalize_Handle(Object: WWW_Type; Handle: WWW_Handle_Type);
+
+   -- TODO: Stopped at raptor_www_write_bytes_handler()
 
 end RDF.Raptor.WWW;
