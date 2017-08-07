53e4f6d2a8b25db46cf22f35015b26312e0734c3
Victor Porton <porton@narod.ru>
2017-08-08 01:20:54 +0300

    rasqal_feature_value_type()

--- ada/src/rdf-rasqal-features.adb
+++ ada/src/rdf-rasqal-features.adb
@@ -1,3 +1,5 @@
+with Interfaces.C; use Interfaces.C;
+
 package body RDF.Rasqal.Features is
 
    function rasqal_feature_from_uri (World: RDF.Rasqal.World.Handle_Type; URI: RDF.Raptor.URI.Handle_Type) return Feature_Type
@@ -7,6 +9,15 @@ package body RDF.Rasqal.Features is
       Value: constant Feature_Type := rasqal_feature_from_uri(Get_Handle(World), Get_Handle(URI));
    begin
       return (if Value'Valid then Value else Unknown);
+   end;
+
+   function rasqal_feature_value_type (Feature: Feature_Type) return int
+      with Import, Convention=>C;
+
+   function Get_Type (Feature: Feature_Type) return Feature_Value_Type is
+      Value_Type: constant int := rasqal_feature_value_type(Feature);
+   begin
+      return (if Value_Type in 0..1 then Feature_Value_Type'Val(Value_Type) else Other);
    end;
 
 end RDF.Rasqal.Features;
--- ada/src/rdf-rasqal-features.ads
+++ ada/src/rdf-rasqal-features.ads
@@ -9,6 +9,12 @@ package RDF.Rasqal.Features is
 
    function Feature_From_URI (World: World_Type_Without_Finalize'Class; URI: URI_Type_Without_Finalize'Class) return Feature_Type;
 
-   -- TODO: stopped at rasqal_feature_value_type ()
+   type Feature_Value_Type is (Other, Integer_Type, String_Type)
+      with Convention=>C;
+   for Feature_Value_Type use (Other=>-1, Integer_Type=>0, String_Type=>1);
+
+   function Get_Type (Feature: Feature_Type) return Feature_Value_Type;
+
+   -- TODO: stopped at rasqal_features_enumerate ()
 
 end RDF.Rasqal.Features;
