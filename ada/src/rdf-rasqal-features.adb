c5035c4b3f1cf2510b80178d7a64c9017e66cea9
Victor Porton <porton@narod.ru>
2017-08-08 00:56:01 +0300

    bug fix

--- ada/src/rdf-rasqal-features.adb
+++ ada/src/rdf-rasqal-features.adb
@@ -1,16 +1,12 @@
 package body RDF.Rasqal.Features is
 
-   -- FIXME: 'Base is not what we need
-   function rasqal_feature_from_uri (World: RDF.Rasqal.World.Handle_Type; URI: RDF.Raptor.URI.Handle_Type) return Feature_Type'Base
+   function rasqal_feature_from_uri (World: RDF.Rasqal.World.Handle_Type; URI: RDF.Raptor.URI.Handle_Type) return Feature_Type
       with Import, Convention=>C;
 
    function Feature_From_URI (World: World_Type_Without_Finalize'Class; URI: URI_Type_Without_Finalize'Class) return Feature_Type is
-      Value: Feature_Type'Base := rasqal_feature_from_uri(Get_Handle(World), Get_Handle(URI));
+      Value: constant Feature_Type := rasqal_feature_from_uri(Get_Handle(World), Get_Handle(URI));
    begin
-      if Feature_Type'Pos(Value) < 0 then
-         Value := Unknown;
-      end if;
-      return Value;
+      return (if Value'Valid then Value else Unknown);
    end;
 
 end RDF.Rasqal.Features;
