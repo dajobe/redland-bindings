977e1719c5c2623382ed73dabad18811b5e35f56
Victor Porton <porton@narod.ru>
2017-08-09 20:39:17 +0300

    started to work on Rasqal data graphs

--- ada/src/rdf-rasqal-data_graph.ads
+++ ada/src/rdf-rasqal-data_graph.ads
@@ -1,3 +1,59 @@
+with Interfaces.C; use Interfaces.C;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
+with RDF.Auxiliary.Handled_Record;
+with RDF.Rasqal.World;
+with RDF.Raptor.URI;
+with RDF.Raptor.IOStream;
+
 package RDF.Rasqal.Data_Graph is
+
+   type Data_Graph_Record is private;
+
+   type Data_Graph_Record_Access is access Data_Graph_Record with Convention=>C;
+
+   package Data_Graph_Handled_Record is new RDF.Auxiliary.Handled_Record(Data_Graph_Record, Data_Graph_Record_Access);
+
+   type Data_Graph_Type_Without_Finalize is new Data_Graph_Handled_Record.Base_Object with null record;
+
+   function Get_World (Graph: Data_Graph_Type_Without_Finalize) return RDF.Rasqal.World.World_Type_Without_Finalize;
+
+   function Get_URI (Graph: Data_Graph_Type_Without_Finalize) return RDF.Raptor.URI.URI_Type_Without_Finalize;
+
+   function Get_Name_URI (Graph: Data_Graph_Type_Without_Finalize) return RDF.Raptor.URI.URI_Type_Without_Finalize;
+
+   type Flags_Type is (None, -- unused
+                       Named,
+                       Background);
+
+   function Get_Flags (Graph: Data_Graph_Type_Without_Finalize) return Flags_Type;
+
+   function Get_Format_Type (Graph: Data_Graph_Type_Without_Finalize) return String;
+
+   function Get_Format_Name (Graph: Data_Graph_Type_Without_Finalize) return String;
+
+   function Get_Format_URI (Graph: Data_Graph_Type_Without_Finalize) return RDF.Raptor.URI.URI_Type;
+
+   function Get_Iostream (Graph: Data_Graph_Type_Without_Finalize) return RDF.Raptor.IOStream.Stream_Type_Without_Finalize;
+
+   function Get_Base_URI (Graph: Data_Graph_Type_Without_Finalize) return RDF.Raptor.URI.URI_Type;
+
+   function Get_Usage_Count (Graph: Data_Graph_Type_Without_Finalize) return Natural;
+
+   -- TODO: Stopped at rasqal_new_data_graph_from_data_graph ()
+
+private
+
+   type Data_Graph_Record is
+      record
+         World: RDF.Rasqal.World.Handle_Type;
+         URI, Name_URI: RDF.Raptor.URI.Handle_Type;
+         Flags: unsigned;
+         Format_Type, Format_Name: chars_ptr;
+         Format_URI: RDF.Raptor.URI.Handle_Type;
+         IOStr: RDF.Raptor.IOStream.Handle_Type;
+         Base_URI: RDF.Raptor.URI.Handle_Type;
+         Usage: int;
+      end record
+      with Convention=>C;
 
 end RDF.Rasqal.Data_Graph;
