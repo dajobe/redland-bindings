e38cd8b6aaf980c2dff6146a68fe8beb591a645e
Victor Porton <porton@narod.ru>
2017-08-10 19:27:31 +0300

    rasqal_new_query() bound

---
+++ ada/src/rdf-rasqal-query.adb
@@ -0,0 +1,21 @@
+with Interfaces.C.Strings; use Interfaces.C.Strings;
+with RDF.Auxiliary.C_String_Holders;
+
+package body RDF.Rasqal.Query is
+
+   function rasqal_new_query (World: RDF.Rasqal.World.Handle_Type; Name, URI: chars_ptr) return Query_Handle_Type
+     with Import, Convention=>C;
+
+   function New_Query (World: RDF.Rasqal.World.World_Type; Name, URI: RDF.Auxiliary.String_Holders.Holder) return Query_Type is
+      use RDF.Auxiliary.C_String_Holders;
+      Name2: chars_ptr := New_String(Name);
+      URI2 : chars_ptr := New_String(URI );
+      use RDF.Rasqal.World;
+      Result: constant Query_Handle_Type := rasqal_new_query(Get_Handle(World), Name2, URI2);
+   begin
+      Free(Name2);
+      Free(URI2);
+      return From_Non_Null_Handle(Result);
+   end;
+
+end RDF.Rasqal.Query;
--- ada/src/rdf-rasqal-query.ads
+++ ada/src/rdf-rasqal-query.ads
@@ -1,7 +1,21 @@
+with RDF.Auxiliary;
+with RDF.Auxiliary.Limited_Handled_Record;
+with RDF.Rasqal.World;
+
 package RDF.Rasqal.Query is
 
    -- This API is partial, as I consider many C functions as internals.
 
-   -- TODO: Stopped here
+   package Query_Handled_Record is new RDF.Auxiliary.Limited_Handled_Record(RDF.Auxiliary.Dummy_Record, RDF.Auxiliary.Dummy_Record_Access);
+
+   subtype Query_Handle_Type is Query_Handled_Record.Access_Type;
+
+   type Query_Type_Without_Finalize is new Query_Handled_Record.Base_Object with null record;
+
+   type Query_Type is new Query_Type_Without_Finalize with null record;
+
+   not overriding function New_Query (World: RDF.Rasqal.World.World_Type; Name, URI: RDF.Auxiliary.String_Holders.Holder) return Query_Type;
+
+   -- TODO: Stopped at rasqal_free_query()
 
 end RDF.Rasqal.Query;
