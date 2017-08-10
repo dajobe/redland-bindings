7e5492c63080d0e2edac61642b65321fc9ab4d7c
Victor Porton <porton@narod.ru>
2017-08-10 17:18:29 +0300

    finished accessor methods for Data_Graph_Type_Without_Finalize

--- ada/src/rdf-rasqal-data_graph.adb
+++ ada/src/rdf-rasqal-data_graph.adb
@@ -1,155 +1,65 @@
 package body RDF.Rasqal.Data_Graph is
 
-   -- FIXME: Rewrite
-
-   ---------------
-   -- Get_World --
-   ---------------
-
-   function Get_World
-     (Graph: Data_Graph_Type_Without_Finalize)
-      return RDF.Rasqal.World.World_Type_Without_Finalize
-   is
+   function Get_World (Graph: Data_Graph_Type_Without_Finalize)
+                       return RDF.Rasqal.World.World_Type_Without_Finalize is
+      use RDF.Rasqal.World;
    begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_World unimplemented");
-      raise Program_Error with "Unimplemented function Get_World";
-      return Get_World (Graph => Graph);
-   end Get_World;
+      return From_Handle(Get_Handle(Graph).World);
+   end;
 
-   -------------
-   -- Get_URI --
-   -------------
-
-   function Get_URI
-     (Graph: Data_Graph_Type_Without_Finalize)
-      return RDF.Raptor.URI.URI_Type_Without_Finalize
-   is
+   function Get_URI (Graph: Data_Graph_Type_Without_Finalize)
+                     return RDF.Raptor.URI.URI_Type_Without_Finalize is
+      use RDF.Raptor.URI;
    begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_URI unimplemented");
-      raise Program_Error with "Unimplemented function Get_URI";
-      return Get_URI (Graph => Graph);
-   end Get_URI;
+      return From_Handle(Get_Handle(Graph).URI);
+   end;
 
-   ------------------
-   -- Get_Name_URI --
-   ------------------
-
-   function Get_Name_URI
-     (Graph: Data_Graph_Type_Without_Finalize)
-      return RDF.Raptor.URI.URI_Type_Without_Finalize
-   is
+   function Get_Name_URI (Graph: Data_Graph_Type_Without_Finalize)
+                     return RDF.Raptor.URI.URI_Type_Without_Finalize is
+      use RDF.Raptor.URI;
    begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_Name_URI unimplemented");
-      raise Program_Error with "Unimplemented function Get_Name_URI";
-      return Get_Name_URI (Graph => Graph);
-   end Get_Name_URI;
+      return From_Handle(Get_Handle(Graph).Name_URI);
+   end;
 
-   ---------------
-   -- Get_Flags --
-   ---------------
+   function Get_Flags (Graph: Data_Graph_Type_Without_Finalize) return Flags_Type is
+     (Flags_Type'Val(Get_Handle(Graph).Flags));
 
-   function Get_Flags
-     (Graph: Data_Graph_Type_Without_Finalize)
-      return Flags_Type
-   is
+   function Get_Format_Type (Graph: Data_Graph_Type_Without_Finalize) return String is
+      (Value(Get_Handle(Graph).Format_Type));
+
+   function Get_Format_Name (Graph: Data_Graph_Type_Without_Finalize) return String is
+      (Value(Get_Handle(Graph).Format_Name));
+
+
+   function Get_Format_URI (Graph: Data_Graph_Type_Without_Finalize) return RDF.Raptor.URI.URI_Type is
+      use RDF.Raptor.URI;
    begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_Flags unimplemented");
-      raise Program_Error with "Unimplemented function Get_Flags";
-      return Get_Flags (Graph => Graph);
-   end Get_Flags;
+      return From_Handle(Get_Handle(Graph).Format_URI);
+   end;
 
-   ---------------------
-   -- Get_Format_Type --
-   ---------------------
 
-   function Get_Format_Type
-     (Graph: Data_Graph_Type_Without_Finalize)
-      return String
-   is
+   function Get_Iostream (Graph: Data_Graph_Type_Without_Finalize)
+                          return RDF.Raptor.IOStream.Stream_Type_Without_Finalize is
+      use RDF.Raptor.IOStream;
    begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_Format_Type unimplemented");
-      raise Program_Error with "Unimplemented function Get_Format_Type";
-      return Get_Format_Type (Graph => Graph);
-   end Get_Format_Type;
-
-   ---------------------
-   -- Get_Format_Name --
-   ---------------------
-
-   function Get_Format_Name
-     (Graph: Data_Graph_Type_Without_Finalize)
-      return String
-   is
-   begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_Format_Name unimplemented");
-      raise Program_Error with "Unimplemented function Get_Format_Name";
-      return Get_Format_Name (Graph => Graph);
-   end Get_Format_Name;
-
-   --------------------
-   -- Get_Format_URI --
-   --------------------
-
-   function Get_Format_URI
-     (Graph: Data_Graph_Type_Without_Finalize)
-      return RDF.Raptor.URI.URI_Type
-   is
-   begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_Format_URI unimplemented");
-      raise Program_Error with "Unimplemented function Get_Format_URI";
-      return Get_Format_URI (Graph => Graph);
-   end Get_Format_URI;
-
-   ------------------
-   -- Get_Iostream --
-   ------------------
-
-   function Get_Iostream
-     (Graph: Data_Graph_Type_Without_Finalize)
-      return RDF.Raptor.IOStream.Stream_Type_Without_Finalize
-   is
-   begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_Iostream unimplemented");
-      raise Program_Error with "Unimplemented function Get_Iostream";
-      return Get_Iostream (Graph => Graph);
-   end Get_Iostream;
+      return From_Handle(Get_Handle(Graph).IOStr);
+   end;
 
    ------------------
    -- Get_Base_URI --
    ------------------
 
-   function Get_Base_URI
-     (Graph: Data_Graph_Type_Without_Finalize)
-      return RDF.Raptor.URI.URI_Type
-   is
+   function Get_Base_URI (Graph: Data_Graph_Type_Without_Finalize) return RDF.Raptor.URI.URI_Type is
+      use RDF.Raptor.URI;
    begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_Base_URI unimplemented");
-      raise Program_Error with "Unimplemented function Get_Base_URI";
-      return Get_Base_URI (Graph => Graph);
-   end Get_Base_URI;
+      return From_Handle(Get_Handle(Graph).Base_URI);
+   end;
 
    ---------------------
    -- Get_Usage_Count --
    ---------------------
 
-   function Get_Usage_Count
-     (Graph: Data_Graph_Type_Without_Finalize)
-      return Natural
-   is
-   begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_Usage_Count unimplemented");
-      raise Program_Error with "Unimplemented function Get_Usage_Count";
-      return Get_Usage_Count (Graph => Graph);
-   end Get_Usage_Count;
+   function Get_Usage_Count (Graph: Data_Graph_Type_Without_Finalize) return Natural is
+      (Natural(Get_Handle(Graph).Usage));
 
 end RDF.Rasqal.Data_Graph;
