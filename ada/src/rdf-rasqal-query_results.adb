a7a6fe625ccb88bccde35b18cf0ec502180761a6
Victor Porton <porton@narod.ru>
2017-08-18 21:38:52 +0300

    misc

--- ada/src/rdf-rasqal-query_results.adb
+++ ada/src/rdf-rasqal-query_results.adb
@@ -1,6 +1,17 @@
+with Interfaces.C; use Interfaces.C;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
+with RDF.Raptor.URI;
 with RDF.Rasqal.Query;
 
 package body RDF.Rasqal.Query_Results is
+
+   procedure rasqal_free_query_results (Handle: Query_Results_Handle_Type)
+     with Import, Convention=>C;
+
+   procedure Finalize_Handle (Object: Query_Results_Type; Handle: Query_Results_Handle_Type) is
+   begin
+      rasqal_free_query_results(Handle);
+   end;
 
    function rasqal_new_query_results2 (World: RDF.Rasqal.World.Handle_Type;
                                        Query: RDF.Rasqal.Query.Query_Handle_Type;
@@ -15,6 +26,25 @@ package body RDF.Rasqal.Query_Results is
       use RDF.Rasqal.World, RDF.Rasqal.Query;
    begin
       return From_Non_Null_Handle(rasqal_new_query_results2(Get_Handle(World), Get_Handle(Query), Kind));
-   end New_Query_Results;
+   end;
+
+   function rasqal_new_query_results_from_string (World: RDF.Rasqal.World.Handle_Type;
+                                                  Kind: Query_Results_Type_Enum;
+                                                  Base_URI: RDF.Raptor.URI.Handle_Type;
+                                                  Value: char_array;
+                                                  Length: size_t)
+                                                  return Query_Results_Handle_Type
+     with Import, Convention=>C;
+
+   function From_String (World: RDF.Rasqal.World.World_Type_Without_Finalize;
+                         Kind: Query_Results_Type_Enum;
+                         Base_URI: RDF.Raptor.URI.URI_Type_Without_Finalize;
+                         Value: String)
+                         return Query_Results_Type is
+      use RDF.Rasqal.World, RDF.Raptor.URI;
+   begin
+      return From_Non_Null_Handle(Rasqal_New_Query_Results_From_String(
+                                  Get_Handle(World), Kind, Get_Handle(Base_URI), To_C(Value, Append_Nul=>False), Value'Length));
+   end;
 
 end RDF.Rasqal.Query_Results;
--- ada/src/rdf-rasqal-query_results.ads
+++ ada/src/rdf-rasqal-query_results.ads
@@ -1,5 +1,6 @@
 with RDF.Auxiliary;
 with RDF.Auxiliary.Limited_Handled_Record;
+with RDF.Raptor.URI;
 with RDF.Rasqal.World;
 limited with RDF.Rasqal.Query;
 
@@ -19,11 +20,21 @@ package RDF.Rasqal.Query_Results is
 
    type Query_Results_Type is new Query_Results_Type_Without_Finalize with null record;
 
+   procedure Finalize_Handle (Object: Query_Results_Type; Handle: Query_Results_Handle_Type);
+
    function New_Query_Results (World: RDF.Rasqal.World.World_Type_Without_Finalize;
                                Query: RDF.Rasqal.Query.Query_Type_Without_Finalize;
                                Kind: Query_Results_Type_Enum)
                                return Query_Results_Type;
 
-   -- TODO: Stopped at rasqal_new_query_results_from_string ()
+   function From_String (World: RDF.Rasqal.World.World_Type_Without_Finalize;
+                         Kind: Query_Results_Type_Enum;
+                         Base_URI: RDF.Raptor.URI.URI_Type_Without_Finalize;
+                         Value: String)
+                         return Query_Results_Type;
+
+   -- function Add_Row is deliberately not implemented
+
+   -- TODO: Stopped at rasqal_query_results_finished ()
 
 end RDF.Rasqal.Query_Results;
