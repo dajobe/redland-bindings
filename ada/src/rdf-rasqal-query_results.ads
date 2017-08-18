5349ebff222f4bfc29716441981ccfeeac428cd6
Victor Porton <porton@narod.ru>
2017-08-18 21:02:09 +0300

    started work on query results

--- ada/src/rdf-rasqal-query_results.ads
+++ ada/src/rdf-rasqal-query_results.ads
@@ -1,3 +1,19 @@
+with RDF.Auxiliary;
+with RDF.Auxiliary.Limited_Handled_Record;
+limited with RDF.Rasqal.Query;
+
 package RDF.Rasqal.Query_Results is
+
+   package Query_Results_Handled_Record is new RDF.Auxiliary.Limited_Handled_Record(RDF.Auxiliary.Dummy_Record, RDF.Auxiliary.Dummy_Record_Access);
+
+   subtype Query_Results_Handle_Type is Query_Results_Handled_Record.Access_Type;
+
+   type Query_Results_Type_Without_Finalize is new Query_Results_Handled_Record.Base_Object with null record;
+
+   type Query_Results_Type is Query_Results_Type_Without_Finalize with null record;
+
+   function New_Query_Results (World: RDF.Rasqal.World.World_Type_Without_Finalize;
+                               Query: RDF.Rasqal.Query.Query_Type_Without_Finalize;
+                               Kind: Query_Results_Type_Enum);
 
 end RDF.Rasqal.Query_Results;
