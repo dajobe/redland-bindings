5eb9ac9b919fb0121067ef9ef1b0d9bc6dbadd31
Victor Porton <porton@narod.ru>
2017-09-12 00:00:15 +0300

    more testing

--- ada/src/test/syntaxes_test.adb
+++ ada/src/test/syntaxes_test.adb
@@ -1,17 +1,24 @@
 with AUnit.Test_Cases;
 with AUnit.Assertions;
 with RDF.Raptor.World;
+with RDF.Rasqal.World;
 with RDF.Raptor.Syntaxes; use RDF.Raptor.Syntaxes;
+with RDF.Rasqal.Syntaxes; use RDF.Rasqal.Syntaxes;
 
 package body Syntaxes_Test is
 
    procedure Test_Enum_Syntaxes (T : in out Test_Cases.Test_Case'Class) is
-      World: RDF.Raptor.World.World_Type;
+      World2: RDF.Rasqal.World.World_Type := RDF.Rasqal.World.Open;
+      World: RDF.Raptor.World.World_Type_Without_Finalize := RDF.Rasqal.World.Get_Raptor(World2);
 
       Iterator: Parser_Description_Iterator := Create_Parser_Descriptions_Iterator(World);
       Cursor: Parser_Description_Cursor := First(Iterator);
       Iterator2: Serializer_Description_Iterator := Create_Serializer_Descriptions_Iterator(World);
       Cursor2: Serializer_Description_Cursor := First(Iterator2);
+      Iterator3: Query_Language_Description_Iterator := Create_Query_Language_Descriptions_Iterator(World2);
+      Cursor3: Query_Language_Description_Cursor := First(Iterator3);
+      Iterator4: Query_Results_Format_Description_Iterator := Create_Query_Results_Format_Descriptions_Iterator(World2);
+      Cursor4: Query_Results_Format_Description_Cursor := First(Iterator4);
    begin
       -- Does not compile with GCC 4.9.1
       while Has_Element(Cursor) loop
@@ -19,6 +26,12 @@ package body Syntaxes_Test is
       end loop;
       while Has_Element(Cursor2) loop
          Cursor2 := Next(Iterator2, Cursor2);
+      end loop;
+      while Has_Element(Cursor3) loop
+         Cursor3 := Next(Iterator3, Cursor3);
+      end loop;
+      while Has_Element(Cursor4) loop
+         Cursor4 := Next(Iterator4, Cursor4);
       end loop;
    end;
 
