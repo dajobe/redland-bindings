13f0a0ce035e527d8d5bb543e5d46515c9504108
Victor Porton <porton@narod.ru>
2017-09-22 14:51:33 +0300

    bug fix

--- ada/src/rdf-rasqal-constants.adb
+++ ada/src/rdf-rasqal-constants.adb
@@ -5,16 +5,16 @@ package body RDF.Rasqal.Constants is
    subtype chars_ptr is Interfaces.C.Strings.chars_ptr;
    function Value(Item : in chars_ptr) return String renames Interfaces.C.Strings.Value;
 
-   rascal_copyright_string: constant chars_ptr with Import, Convention=>C;
-   rascal_home_url_string: constant chars_ptr with Import, Convention=>C;
-   rascal_license_string: constant chars_ptr with Import, Convention=>C;
-   rascal_short_copyright_string: constant chars_ptr with Import, Convention=>C;
-   rascal_version_string: constant chars_ptr with Import, Convention=>C;
+   rasqal_copyright_string: constant chars_ptr with Import, Convention=>C;
+   rasqal_home_url_string: constant chars_ptr with Import, Convention=>C;
+   rasqal_license_string: constant chars_ptr with Import, Convention=>C;
+   rasqal_short_copyright_string: constant chars_ptr with Import, Convention=>C;
+   rasqal_version_string: constant chars_ptr with Import, Convention=>C;
 
-   function copyright_string return String is (Value(rascal_copyright_string));
-   function home_url_string return String is (Value(rascal_home_url_string));
-   function license_string return String is (Value(rascal_license_string));
-   function short_copyright_string return String is (Value(rascal_short_copyright_string));
-   function version_string return String is (Value(rascal_version_string));
+   function copyright_string return String is (Value(rasqal_copyright_string));
+   function home_url_string return String is (Value(rasqal_home_url_string));
+   function license_string return String is (Value(rasqal_license_string));
+   function short_copyright_string return String is (Value(rasqal_short_copyright_string));
+   function version_string return String is (Value(rasqal_version_string));
 
 end RDF.Rasqal.Constants;
