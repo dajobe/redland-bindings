e245f359ec251f252a4bb71b32aa34933196b4b3
Victor Porton <porton@narod.ru>
2017-10-05 19:35:52 +0300

    set/get raptor/rasqal

--- ada/src/rdf-redland-world.adb
+++ ada/src/rdf-redland-world.adb
@@ -32,4 +32,42 @@ package body RDF.Redland.World is
       end return;
    end;
 
+   procedure librdf_world_set_rasqal (World: Redland_World_Handle; Rasqal_World: Rasqal_World_Handle)
+     with Import, Convention=>C;
+
+   procedure Set_Rasqal (World: Redland_World_Type_Without_Finalize;
+                         Rasqal_World: Rasqal_World_Type_Without_Finalize'Class) is
+   begin
+      librdf_world_set_rasqal(Get_Handle(World), Get_Handle(Rasqal_World));
+   end;
+
+   function librdf_world_get_rasqal (World: Redland_World_Handle) return Rasqal_World_Handle
+     with Import, Convention=>C;
+
+   function Get_Rasqal (World: Redland_World_Type_Without_Finalize)
+                        return Rasqal_World_Type_Without_Finalize is
+   begin
+      return From_Non_Null_Handle(librdf_world_get_rasqal(Get_Handle(World)));
+   end;
+
+   ----
+
+   procedure librdf_world_set_raptor (World: Redland_World_Handle; Raptor_World: Raptor_World_Handle)
+     with Import, Convention=>C;
+
+   procedure Set_Raptor (World: Redland_World_Type_Without_Finalize;
+                         Raptor_World: Raptor_World_Type_Without_Finalize'Class) is
+   begin
+      librdf_world_set_raptor(Get_Handle(World), Get_Handle(Raptor_World));
+   end;
+
+   function librdf_world_get_raptor (World: Redland_World_Handle) return Raptor_World_Handle
+     with Import, Convention=>C;
+
+   function Get_Raptor (World: Redland_World_Type_Without_Finalize)
+                        return Raptor_World_Type_Without_Finalize is
+   begin
+      return From_Non_Null_Handle(librdf_world_get_raptor(Get_Handle(World)));
+   end;
+
 end RDF.Redland.World;
--- ada/src/rdf-redland-world.ads
+++ ada/src/rdf-redland-world.ads
@@ -1,4 +1,6 @@
 with RDF.Auxiliary.Limited_Handled_Record;
+with RDF.Raptor.World; use RDF.Raptor.World;
+with RDF.Rasqal.World; use RDF.Rasqal.World;
 
 package RDF.Redland.World is
 
@@ -13,12 +15,24 @@ package RDF.Redland.World is
 
    not overriding procedure Open (Object: in out Redland_World_Type_Without_Finalize);
 
+   not overriding procedure Set_Rasqal (World: Redland_World_Type_Without_Finalize;
+                                        Rasqal_World: Rasqal_World_Type_Without_Finalize'Class);
+
+   not overriding function Get_Rasqal (World: Redland_World_Type_Without_Finalize)
+                                       return Rasqal_World_Type_Without_Finalize;
+
+   not overriding procedure Set_Raptor (World: Redland_World_Type_Without_Finalize;
+                                        Raptor_World: Raptor_World_Type_Without_Finalize'Class);
+
+   not overriding function Get_Raptor (World: Redland_World_Type_Without_Finalize)
+                                       return Raptor_World_Type_Without_Finalize;
+
    type Redland_World_Type is new Redland_World_Type_Without_Finalize with null record;
 
    overriding procedure Finalize_Handle (Object: Redland_World_Type; Handle: Redland_World_Handle);
 
    not overriding function Open return Redland_World_Type;
 
-   -- TODO: Stopped at librdf_world_open()
+   -- TODO: Stopped at librdf_world_set_error()
 
 end RDF.Redland.World;
