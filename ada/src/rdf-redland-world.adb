76729871a4b49636ca1e937b52de08e834c82977
Victor Porton <porton@narod.ru>
2017-10-05 19:23:46 +0300

    World.Open

--- ada/src/rdf-redland-world.adb
+++ ada/src/rdf-redland-world.adb
@@ -17,4 +17,19 @@ package body RDF.Redland.World is
       librdf_free_world(Handle);
    end;
 
+   procedure librdf_world_open (Handle: Redland_World_Handle)
+     with Import, Convention=>C;
+
+   procedure Open (Object: in out Redland_World_Type_Without_Finalize) is
+   begin
+      librdf_world_open(Get_Handle(Object));
+   end;
+
+   function Open return Redland_World_Type is
+   begin
+      return Object: Redland_World_Type do
+         Open(Object);
+      end return;
+   end;
+
 end RDF.Redland.World;
--- ada/src/rdf-redland-world.ads
+++ ada/src/rdf-redland-world.ads
@@ -11,9 +11,13 @@ package RDF.Redland.World is
    overriding function Default_Handle(Object: Redland_World_Type_Without_Finalize)
                                       return Redland_World_Handle;
 
+   not overriding procedure Open (Object: in out Redland_World_Type_Without_Finalize);
+
    type Redland_World_Type is new Redland_World_Type_Without_Finalize with null record;
 
    overriding procedure Finalize_Handle (Object: Redland_World_Type; Handle: Redland_World_Handle);
+
+   not overriding function Open return Redland_World_Type;
 
    -- TODO: Stopped at librdf_world_open()
 
