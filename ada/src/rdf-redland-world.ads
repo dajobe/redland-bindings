330b69cacdbf59ae06ae3834633dd9413728b858
Victor Porton <porton@narod.ru>
2017-10-07 19:08:35 +0300

    working

--- ada/src/rdf-redland-world.ads
+++ ada/src/rdf-redland-world.ads
@@ -4,6 +4,9 @@ with RDF.Rasqal.World; use RDF.Rasqal.World;
 
 package RDF.Redland.World is
 
+   Feature_Genid_Base: constant String := "http://feature.librdf.org/genid-base";
+   Feature_Genid_Counter: constant String := "http://feature.librdf.org/genid-counter";
+
    package Redland_World_Handled_Record is new RDF.Auxiliary.Limited_Handled_Record(RDF.Auxiliary.Dummy_Record, RDF.Auxiliary.Dummy_Record_Access);
 
    type Redland_World_Type_Without_Finalize is new Redland_World_Handled_Record.Base_Object with null record;
@@ -29,6 +32,9 @@ package RDF.Redland.World is
 
    not overriding procedure Set_Digest (World: Redland_World_Type_Without_Finalize; Name: String);
 
+   -- TODO: Stopped at librdf_world_get_feature(), will continue after RDF.Redland.Node
+--     not overriding function Get_Feature (World: Redland_World_Type_Without_Finalize; Feature: URI_Type_Without_Finalize'Class)
+
    type Redland_World_Type is new Redland_World_Type_Without_Finalize with null record;
 
    overriding procedure Finalize_Handle (Object: Redland_World_Type; Handle: Redland_World_Handle);
@@ -37,6 +43,8 @@ package RDF.Redland.World is
 
    -- librdf_world_set_error() and librdf_world_set_warning() deliberately not implemented.
 
-   -- TODO: Stopped at librdf_raptor_init_handler()
+   -- I deliberately not implement librdf_world_set_raptor_init_handler() and
+   -- librdf_world_set_rasqal_init_handler().
+   -- I recommend to use Set_Raptor and Set_Rasqal instead.
 
 end RDF.Redland.World;
