e4270f5baa7a7969574a2a45ae786aafc55804ae
Victor Porton <porton@narod.ru>
2017-10-10 00:46:18 +0300

    Revert "API improvement"
    This reverts commit c9de41eab2d3035daeaadf541466b19676d0575c.

--- ada/src/rdf-redland-uri.ads
+++ ada/src/rdf-redland-uri.ads
@@ -18,10 +18,10 @@ package RDF.Redland.URI is
 
    not overriding procedure Print (URI: URI_Type_Without_Finalize; File: RDF.Auxiliary.C_File_Access);
 
-   function Equals (Left, Right: URI_Type_Without_Finalize'Class) return Boolean;
+   not overriding function Equals (Left, Right: URI_Type_Without_Finalize) return Boolean;
 
-   function "=" (Left, Right: URI_Type_Without_Finalize'Class) return Boolean
-                 renames Equals;
+   overriding function "=" (Left, Right: URI_Type_Without_Finalize) return Boolean
+                            renames Equals;
 
    -- TODO: Create subtypes with dynamic predicate?
    not overriding function Is_File_URI (URI: URI_Type_Without_Finalize) return Boolean;
