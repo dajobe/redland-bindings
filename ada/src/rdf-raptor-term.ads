e7ab958189aed5e6418fe11293186113a6392f65
Victor Porton <porton@narod.ru>
2017-10-10 23:26:46 +0300

    TODO comments

--- ada/src/rdf-raptor-term.ads
+++ ada/src/rdf-raptor-term.ads
@@ -15,7 +15,7 @@ package RDF.Raptor.Term is
 
    package Term_Handled_Record is new RDF.Auxiliary.Handled_Record(Term_Record, Term_Record_Access);
 
-   -- TODO: Add subtypes with dynamic predicates
+   -- TODO: Add subtypes with dynamic predicates (first make Is_* functions)
    type Term_Type_Without_Finalize is new Term_Handled_Record.Base_Object with null record;
 
    subtype Term_Handle is Term_Handled_Record.Access_Type;
