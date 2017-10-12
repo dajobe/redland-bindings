8f3dac2f329cb342bdd778845a186641b441023b
Victor Porton <porton@narod.ru>
2017-10-12 20:19:39 +0300

    started work on Model

--- ada/src/rdf-redland-model.ads
+++ ada/src/rdf-redland-model.ads
@@ -1,5 +1,16 @@
+with RDF.Auxiliary.Limited_Handled_Record;
+
 package RDF.Redland.Model is
 
-   -- Stopped here (will continue after Iterator)
+   -- Use Limited_Handled_Record because despite a model can be copied,
+   -- copying may be a very expensive operation. Use Copy function instead.
+   -- See also https://gcc.gnu.org/bugzilla/show_bug.cgi?id=62042
+   package Model_Handled_Record is new RDF.Auxiliary.Limited_Handled_Record(RDF.Auxiliary.Dummy_Record, RDF.Auxiliary.Dummy_Record_Access);
+
+   type Model_Type_Without_Finalize is new Model_Handled_Record.Base_Object with null record;
+
+   subtype Model_Handle is Model_Handled_Record.Access_Type;
+
+   -- Stopped here
 
 end RDF.Redland.Model;
