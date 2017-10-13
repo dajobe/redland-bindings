aa5be666a4a8c6b072927ffde7e2e37f6cd16916
Victor Porton <porton@narod.ru>
2017-10-13 20:08:14 +0300

    Statement part flags

--- ada/src/rdf-redland-statement.ads
+++ ada/src/rdf-redland-statement.ads
@@ -11,4 +11,13 @@ package RDF.Redland.Statement is
 
    not overriding function From_Raptor (Statement: RDF.Raptor.Statement.Statement_Type_Without_Finalize'Class) return Statement_Type_Without_Finalize;
 
+   type Statement_Part_Flags is mod 256; -- the number may change in a future version
+
+   Subject_Part  : Statement_Part_Flags := 1;
+   Predicate_Part: Statement_Part_Flags := 2;
+   Object_Part   : Statement_Part_Flags := 4;
+   All_Parts     : Statement_Part_Flags := Subject_Part or Predicate_Part or Object_Part;
+
+   -- Stopped at librdf_new_statement()
+
 end RDF.Redland.Statement;
