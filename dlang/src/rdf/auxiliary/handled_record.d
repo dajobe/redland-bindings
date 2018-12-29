040a65522a52e5ee6dc2dc6c88e22cd8f15a9c83
Victor Porton <porton@narod.ru>
2018-12-29 09:28:41 +0200

    destructor and constructor arguments

--- dlang/src/rdf/auxiliary/handled_record.d
+++ dlang/src/rdf/auxiliary/handled_record.d
@@ -3,9 +3,10 @@ struct Dummy {
     private char dummy = 0; // C99 requires at least one member in struct
 }
 
-alias void function (Dummy* ptr) Destructor;
+alias extern(C) void function (Dummy* ptr) Destructor;
+alias extern(C) Dummy* function () Constructor;
 
-template CObject(Destructor destructor) {
+template CObject(Destructor destructor, Constructor constructor = null) {
     struct BaseObject {
         private Dummy *obj;
 
