20dad56b7b54173e650d2c496c9f6a5ad491fc7e
Victor Porton <porton@narod.ru>
2018-12-29 10:17:12 +0200

    working on base structs

--- dlang/src/rdf/auxiliary/handled_record.d
+++ dlang/src/rdf/auxiliary/handled_record.d
@@ -1,3 +1,6 @@
+class RDFException: Exception { }
+class NonNullRDFException: RDFException { }
+
 extern(C)
 struct Dummy {
     private char dummy = 0; // C99 requires at least one member in struct
@@ -7,36 +10,43 @@ alias extern(C) void function (Dummy* ptr) Destructor;
 alias extern(C) Dummy* function () Constructor;
 
 template CObject(Destructor destructor, Constructor constructor = null) {
-    struct BaseObject {
-        private Dummy *ptr;
+    struct WithoutFinalization {
+        private Dummy* ptr;
         static if (constructor) {
             this() {
                 ptr = constructor();
             }
         }
-        this(Dummy *ptr) {
+        this(Dummy* ptr) {
             this.ptr = ptr;
         }
         ~this() {
             destructor(ptr);
         }
+        @property Dummy* handle() {
+            return ptr;
+        }
+        static from_nonnull_handle(Dummy* ptr) {
+            if(!ptr) throw NonNullRDFException;
+            return WithoutFinalization(ptr);
+        }
     }
 
     struct WithFinalization {
-        private Dummy *ptr;
+        private Dummy* ptr;
         static if (constructor) {
             this() {
                 ptr = constructor();
             }
         }
-        this(Dummy *ptr) {
+        this(Dummy* ptr) {
             this.ptr = ptr;
         }
         ~this() {
             destructor(ptr);
         }
         private @property void base() {
-            return BaseObject(ptr);
+            return WithoutFinalization(ptr);
         }
         alias base this;
     }
