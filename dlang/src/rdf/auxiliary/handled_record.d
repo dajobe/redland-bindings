607036e1011ac70eadf2f83fb7d4a0bbed1908cf
Victor Porton <porton@narod.ru>
2018-12-29 09:38:49 +0200

    the CObject template base structure

--- dlang/src/rdf/auxiliary/handled_record.d
+++ dlang/src/rdf/auxiliary/handled_record.d
@@ -8,7 +8,36 @@ alias extern(C) Dummy* function () Constructor;
 
 template CObject(Destructor destructor, Constructor constructor = null) {
     struct BaseObject {
-        private Dummy *obj;
+        private Dummy *ptr;
+        static if (constructor) {
+            this() {
+                ptr = constructor();
+            }
+        }
+        this(Dummy *ptr) {
+            this.ptr = ptr;
+        }
+        ~this() {
+            destructor(ptr);
+        }
+    }
 
+    struct WithFinalization {
+        private Dummy *ptr;
+        static if (constructor) {
+            this() {
+                ptr = constructor();
+            }
+        }
+        this(Dummy *ptr) {
+            this.ptr = ptr;
+        }
+        ~this() {
+            destructor(ptr);
+        }
+        private @property void base() {
+            return BaseObject(ptr);
+        }
+        alias base this;
     }
 }
