236a014bef0943e11b5c68d9049a6d39a30e86b4
Victor Porton <porton@narod.ru>
2018-12-31 23:22:57 +0200

    toRelativeURIString

--- dlang/source/rdf/raptor/uri.d
+++ dlang/source/rdf/raptor/uri.d
@@ -2,6 +2,7 @@ module rdf.raptor.uri;
 
 import std.string;
 import rdf.auxiliary.handled_record;
+import rdf.raptor.memory;
 
 private extern extern(C) {
     void raptor_free_uri(Dummy* uri);
@@ -9,6 +10,7 @@ private extern extern(C) {
     int raptor_uri_compare(Dummy* uri1, Dummy* uri2);
     int raptor_uri_equals(Dummy* uri1, Dummy* uri2);
     char* raptor_uri_as_string(Dummy* uri);
+    char* raptor_uri_to_relative_uri_string(Dummy* base_uri, Dummy* reference_uri);
 }
 
 /// Only absolute URIs!
@@ -19,6 +21,13 @@ struct URIWithoutFinalize {
     mixin CompareHandles!(raptor_uri_equals, raptor_uri_compare);
     string toString() {
         return fromStringz(raptor_uri_as_string(handle)).idup;
+    }
+    static string toRelativeURIString(URIWithoutFinalize baseURI, URIWithoutFinalize referenceURI) {
+      char* str =
+            raptor_uri_to_relative_uri_string(baseURI.handle, referenceURI.handle);
+      if(!str) throw new NullRDFException();
+      scope(exit) raptor_free_memory(str);
+      return fromStringz(str).idup;
     }
 }
 
