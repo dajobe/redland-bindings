2d37a18e2ee5ddc9ab841d32e5f4e80ab2a6252f
Victor Porton <porton@narod.ru>
2018-12-31 20:02:02 +0200

    working on bnode.d

---
+++ dlang/source/rdf/auxiliary/nullable_string.d
@@ -0,0 +1,8 @@
+module rdf.auxiliary.nullable_string;
+
+import std.typecons;
+import std.string;
+
+immutable(char)* myToStringz(Nullable!string s) {
+  return s.isNull ? null : toStringz(s.get);
+}
--- dlang/source/rdf/raptor/bnode.d
+++ dlang/source/rdf/raptor/bnode.d
@@ -1,6 +1,8 @@
 module rdf.raptor.bnode;
 
 import std.string;
+import std.typecons;
+import rdf.auxiliary.nullable_string;
 import rdf.auxiliary.handled_record;
 import rdf.auxiliary.user;
 import rdf.raptor.memory;
@@ -8,6 +10,11 @@ import rdf.raptor.world;
 
 private extern extern(C) {
     char* raptor_world_generate_bnodeid(Dummy* world);
+    alias raptor_generate_bnodeid_handler = extern(C) char* function(char *data, char* userID);
+    void raptor_world_set_generate_bnodeid_handler(Dummy* world,
+                                                   void *user_data,
+                                                   raptor_generate_bnodeid_handler handler);
+    void raptor_world_set_generate_bnodeid_parameters(Dummy* world, const char *prefix, int base);
 }
 
 struct BNode {
@@ -22,6 +29,12 @@ struct BNode {
             raptor_free_memory(str);
         }
         return dup;
+    }
+    static void setGenerateBnodeidParameters(RaptorWorldWithoutFinalize world,
+                                             Nullable!string prefix,
+                                             int base = 1)
+    {
+        raptor_world_set_generate_bnodeid_parameters(world.handle, myToStringz(prefix), base);
     }
 }
 
