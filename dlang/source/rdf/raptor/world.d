e7312e5064c38b4d0a5aba441b3f7f923d0b1d9c
Victor Porton <porton@narod.ru>
2018-12-31 07:50:17 +0200

    source code reorder

--- dlang/source/rdf/raptor/world.d
+++ dlang/source/rdf/raptor/world.d
@@ -2,6 +2,20 @@ module rdf.raptor.world;
 
 import rdf.auxiliary.handled_record;
 
+enum RaptorFlagType : char { LibxmlErrorSave = 1,
+                             LibxmlStructuredErrorSave = 2,
+                             URIInterning = 3,
+                             WWWSkipInitFinish = 4 }
+
+struct FlagAndValue {
+    RaptorFlagType flag;
+    bool value;
+}
+
+unittest {
+    assert(1);
+}
+
 private extern extern(C) Dummy* raptor_new_world();
 private extern extern(C) void raptor_free_world(Dummy* world);
 
@@ -17,20 +31,6 @@ struct RaptorWorld {
                             RaptorWorld,
                             raptor_free_world,
                             raptor_new_world);
-}
-
-enum RaptorFlagType : char { LibxmlErrorSave = 1,
-                             LibxmlStructuredErrorSave = 2,
-                             URIInterning = 3,
-                             WWWSkipInitFinish = 4 }
-
-struct FlagAndValue {
-    RaptorFlagType flag;
-    bool value;
-}
-
-unittest {
-    assert(1);
 }
 
 // TODO
