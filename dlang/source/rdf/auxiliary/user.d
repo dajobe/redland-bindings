24363599fcc446d1e2f58e38be472ee3431c272d
Victor Porton <porton@narod.ru>
2018-12-31 19:10:30 +0200

    removed wrong code

--- dlang/source/rdf/auxiliary/user.d
+++ dlang/source/rdf/auxiliary/user.d
@@ -5,11 +5,9 @@ import core.memory : GC;
 /// Internal
 class UnmovableObject {
     this() {
-        //GC.addRoot(cast(void*)this); // prevents the destructor to work
         GC.setAttr(cast(void*)this, GC.BlkAttr.NO_MOVE);
     }
     ~this() {
-       //GC.removeRoot(cast(void*)this);
        GC.clrAttr(cast(void*)this, GC.BlkAttr.NO_MOVE);
    }
 }
