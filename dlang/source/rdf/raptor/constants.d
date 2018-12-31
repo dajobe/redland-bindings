c6946d1bd65060e6d3fa99e6d28876fb64ea63ac
Victor Porton <porton@narod.ru>
2018-12-31 11:52:57 +0200

    raptor constants.d finished

--- dlang/source/rdf/raptor/constants.d
+++ dlang/source/rdf/raptor/constants.d
@@ -10,4 +10,31 @@ alias version_minor   = raptor_version_minor;
 alias version_release = raptor_version_release;
 alias version_decimal = raptor_version_decimal;
 
-// TODO
+private extern extern(C) {
+    string raptor_copyright_string;
+    string raptor_home_url_string;
+    string raptor_license_string;
+    string raptor_short_copyright_string;
+    string raptor_version_string;
+    string raptor_owl_namespace_uri;
+    string raptor_rdf_namespace_uri;
+    string raptor_rdf_schema_namespace_uri;
+    string raptor_xml_literal_datatype_uri_string;
+    string raptor_xml_namespace_uri;
+    string raptor_xmlschema_datatypes_namespace_uri;
+}
+
+alias copyright_string = raptor_copyright_string;
+alias home_url_string = raptor_home_url_string;
+alias license_string = raptor_license_string;
+alias short_copyright_string = raptor_short_copyright_string;
+alias version_string = raptor_version_string;
+alias owl_namespace_uri = raptor_owl_namespace_uri;
+alias rdf_namespace_uri = raptor_rdf_namespace_uri;
+alias rdf_schema_namespace_uri = raptor_rdf_schema_namespace_uri;
+alias xml_literal_datatype_uri_string = raptor_xml_literal_datatype_uri_string;
+alias xml_namespace_uri = raptor_xml_namespace_uri;
+alias xmlschema_datatypes_namespace_uri = raptor_xmlschema_datatypes_namespace_uri;
+
+extern extern(C) uint rdf_namespace_uri_len;
+extern extern(C) uint xml_literal_datatype_uri_string_len;
