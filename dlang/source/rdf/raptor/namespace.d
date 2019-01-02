a6a49ceb7eec6457d69450d17e0951477397e031
Victor Porton <porton@narod.ru>
2019-01-02 11:47:41 +0200

    work on namespace.d

--- dlang/source/rdf/raptor/namespace.d
+++ dlang/source/rdf/raptor/namespace.d
@@ -12,12 +12,21 @@ struct NamespaceHandle;
 
 private extern extern(C) {
     void raptor_free_namespace(NamespaceHandle* ns);
+    URIHandle* raptor_namespace_get_uri(const NamespaceHandle* ns);
+    const(char*) raptor_namespace_get_prefix(const NamespaceHandle* ns);
 }
 
 struct NamespaceWithoutFinalize {
     mixin WithoutFinalize!(NamespaceHandle,
                            NamespaceWithoutFinalize,
                            Namespace);
+    @property URIWithoutFinalize uri() {
+        // raptor_namespace_get_uri() may return NULL (for xmlns="")
+        return URIWithoutFinalize.fromHandle(raptor_namespace_get_uri(handle));
+    }
+    @property string prefix() {
+        return raptor_namespace_get_prefix(handle).fromStringz.idup;
+    }
 }
 
 struct Namespace {
@@ -26,3 +35,5 @@ struct Namespace {
                         Namespace,
                         raptor_free_namespace);
 }
+
+// TOOD: Stopped at Write
