5cdc9f6cc5673fe5b4572bf6f06dd9f44481192d
Victor Porton <porton@narod.ru>
2019-01-02 12:17:39 +0200

    namespace.d implemented

--- dlang/source/rdf/raptor/namespace.d
+++ dlang/source/rdf/raptor/namespace.d
@@ -4,9 +4,10 @@ import std.string;
 // import std.typecons;
 import rdf.auxiliary.handled_record;
 // import rdf.auxiliary.nullable_string;
-// import rdf.raptor.memory;
+import rdf.raptor.memory;
 import rdf.raptor.world;
 import rdf.raptor.uri;
+import rdf.raptor.iostream;
 
 struct NamespaceHandle;
 
@@ -14,6 +15,14 @@ private extern extern(C) {
     void raptor_free_namespace(NamespaceHandle* ns);
     URIHandle* raptor_namespace_get_uri(const NamespaceHandle* ns);
     const(char*) raptor_namespace_get_prefix(const NamespaceHandle* ns);
+    int raptor_namespace_write(NamespaceHandle* ns, IOStreamHandle* iostr);
+    char* raptor_namespace_format_as_xml(const NamespaceHandle* ns, size_t* length_p);
+    int raptor_xml_namespace_string_parse(const char *string, char **prefix, char **uri_string);
+}
+
+struct PrefixAndURI {
+    string prefix;
+    string uri;
 }
 
 struct NamespaceWithoutFinalize {
@@ -27,6 +36,16 @@ struct NamespaceWithoutFinalize {
     @property string prefix() {
         return raptor_namespace_get_prefix(handle).fromStringz.idup;
     }
+    void write(IOStreamWithoutFinalize stream) {
+        if(raptor_namespace_write(handle, stream.handle) != 0)
+          throw new IOStreamException();
+    }
+    string formatAsXML() {
+      char* str = raptor_namespace_format_as_xml(handle, null);
+      if(!str) throw new RDFException();
+      scope(exit) raptor_free_memory(str);
+      return str.fromStringz.idup;
+    }
 }
 
 struct Namespace {
@@ -34,6 +53,40 @@ struct Namespace {
                         NamespaceWithoutFinalize,
                         Namespace,
                         raptor_free_namespace);
+    // TODO:
+//     Namespace Create(NamespaceStackWithoutFinalize stack, string prefix, string ns, uint depth) {
+//     }
+//     Namespace fromURI(NamespaceStackWithoutFinalize stack, string prefix, URIWithoutFinalize uri, int depth) {
+//     }
 }
 
-// TOOD: Stopped at Write
+// See also extractPrefix and extractURI
+PrefixAndURI stringParse(string ns) {
+    char* prefix, uri;
+    if(raptor_xml_namespace_string_parse(ns.toStringz, &prefix, &uri) != 0)
+        throw new RDFException();
+    scope(exit) {
+        raptor_free_memory(prefix);
+        raptor_free_memory(uri);
+    }
+    string prefix2 = prefix.fromStringz.idup;
+    string uri2 = uri.fromStringz.idup;
+    return PrefixAndURI(prefix2, uri2);
+}
+
+string extractPrefix(string ns) {
+    char* prefix;
+    if(raptor_xml_namespace_string_parse(ns.toStringz, &prefix, null) != 0)
+        throw new RDFException();
+    scope(exit) raptor_free_memory(prefix);
+    string result = prefix.fromStringz.idup;
+    return result;
+}
+
+string extractURI(string ns) {
+    char* uri;
+    if(raptor_xml_namespace_string_parse(ns.toStringz, null, &uri) != 0)
+        throw new RDFException();
+    scope(exit) raptor_free_memory(uri);
+    return uri.fromStringz.idup;
+}
