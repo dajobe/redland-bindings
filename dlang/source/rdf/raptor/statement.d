a9c82f9bf8c6c53bb4bf3473593ecc153b050cbc
Victor Porton <porton@narod.ru>
2019-01-03 10:32:54 +0200

    @property world

--- dlang/source/rdf/raptor/statement.d
+++ dlang/source/rdf/raptor/statement.d
@@ -4,9 +4,15 @@ import std.string;
 import rdf.auxiliary.handled_record;
 import rdf.raptor.memory;
 import rdf.raptor.world;
-// import rdf.raptor.uri;
+import rdf.raptor.term;
 
-struct StatementHandle;
+extern(C)
+struct StatementHandle {
+private:
+    RaptorWorldHandle* _world;
+    int _usage;
+    TermHandle _subject, _predicate, _object, _graph;
+}
 
 private extern extern(C) {
     StatementHandle* raptor_statement_copy(StatementHandle* statement);
@@ -18,6 +24,9 @@ struct StatementWithoutFinalize {
                            StatementWithoutFinalize,
                            Statement,
                            raptor_statement_copy);
+    @property RaptorWorldWithoutFinalize world() {
+        return RaptorWorldWithoutFinalize.fromNonnullHandle(handle._world);
+    }
 }
 
 struct Statement {
@@ -26,3 +35,5 @@ struct Statement {
                         Statement,
                         raptor_free_statement);
 }
+
+// TODO: Stopped at Get_Subject
