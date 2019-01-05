7e4866ddac13ff519d58dde5ccca6444871834f1
Victor Porton <porton@narod.ru>
2019-01-05 10:14:43 +0200

    working on iostream.d

--- dlang/source/rdf/raptor/iostream.d
+++ dlang/source/rdf/raptor/iostream.d
@@ -6,9 +6,10 @@ struct IOStreamHandle;
 
 private extern extern(C) {
     void raptor_free_iostream(IOStreamHandle* iostr);
+    int raptor_iostream_hexadecimal_write(uint integer, int width, IOStreamHandle* iostr);
 }
 
-// TODO: Make this instead wrapper over D streams
+// TODO: Make this instead wrapper over D streams: https://stackoverflow.com/a/54029257/856090
 
 class IOStreamException: Exception {
     this(string msg, string file = __FILE__, size_t line = __LINE__) {
@@ -23,6 +24,10 @@ struct IOStreamWithoutFinalize {
     mixin WithoutFinalize!(IOStreamHandle,
                            IOStreamWithoutFinalize,
                            IOStream);
+    void hexadecimalWrite(uint value, uint width) {
+        if(raptor_iostream_hexadecimal_write(value, width, handle) < 0)
+            throw new IOStreamException();
+    }
 }
 
 struct IOStream {
@@ -32,4 +37,4 @@ struct IOStream {
                         raptor_free_iostream);
 }
 
-// TODO: Stopped at procedure Hexadecimal_Write
+// TODO: Stopped at procedure Read_Bytes
