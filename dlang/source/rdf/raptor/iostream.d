cd93de1a9b7200b76710ac5454cb4187a95acf17
Victor Porton <porton@narod.ru>
2019-01-05 11:23:09 +0200

    some functions

--- dlang/source/rdf/raptor/iostream.d
+++ dlang/source/rdf/raptor/iostream.d
@@ -7,6 +7,8 @@ struct IOStreamHandle;
 private extern extern(C) {
     void raptor_free_iostream(IOStreamHandle* iostr);
     int raptor_iostream_hexadecimal_write(uint integer, int width, IOStreamHandle* iostr);
+    int raptor_iostream_read_bytes(void *ptr, size_t size, size_t nmemb, IOStreamHandle* iostr);
+    int raptor_iostream_read_eof(IOStreamHandle* iostr);
 }
 
 // TODO: Make this instead wrapper over D streams: https://stackoverflow.com/a/54029257/856090
@@ -28,6 +30,14 @@ struct IOStreamWithoutFinalize {
         if(raptor_iostream_hexadecimal_write(value, width, handle) < 0)
             throw new IOStreamException();
     }
+    size_t readBytes (char *ptr, size_t size, size_t nmemb) {
+        immutable int result = raptor_iostream_read_bytes(ptr, size, nmemb, handle);
+        if(result < 0) throw new IOStreamException();
+        return result;
+    }
+    @property bool eof() {
+        return raptor_iostream_read_eof(handle) != 0;
+    }
 }
 
 struct IOStream {
@@ -37,4 +47,4 @@ struct IOStream {
                         raptor_free_iostream);
 }
 
-// TODO: Stopped at procedure Read_Bytes
+// TODO: Stopped at function Tell
