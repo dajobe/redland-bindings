c5f58b0db9f7b754ad04588f511023bef2e9553c
Victor Porton <porton@narod.ru>
2019-01-07 20:25:12 +0200

    started work on serializer.d

---
+++ dlang/source/rdf/raptor/serializer.d
@@ -0,0 +1,24 @@
+module rdf.raptor.serializer;
+
+import std.string;
+import std.stdio : FILE, File;
+import rdf.auxiliary.handled_record;
+
+struct SerializerHandle;
+
+private extern extern(C) {
+    void raptor_free_serializer(SerializerHandle* rdf_serializer);
+}
+
+struct SerializerWithoutFinalize {
+    mixin WithoutFinalize!(SerializerHandle,
+                           SerializerWithoutFinalize,
+                           Serializer);
+}
+
+struct Serializer {
+    mixin WithFinalize!(SerializerHandle,
+                        SerializerWithoutFinalize,
+                        Serializer,
+                        raptor_free_serializer);
+}
