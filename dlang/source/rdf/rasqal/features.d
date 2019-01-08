8c7e714b061e1b5aef920e59c5d473e72f2ca368
Victor Porton <porton@narod.ru>
2019-01-08 21:45:09 +0200

    working on rdf/rasqal/features.d

--- dlang/source/rdf/rasqal/features.d
+++ dlang/source/rdf/rasqal/features.d
@@ -5,12 +5,19 @@ import rdf.rasqal.world;
 
 enum FeatureType { Unknown = -1, No_Net = 0, Rand_Seed = 1 }
 
+enum FeatureValueType { Other = -1, Integer_Type = 0, String_Type = 1 }
+
 private extern extern(C) {
     FeatureType rasqal_feature_from_uri(RasqalWorldHandle* world, URIHandle* uri);
+    int rasqal_feature_value_type(const FeatureType feature);
 }
 
 FeatureType Feature_From_URI(RasqalWorldWithoutFinalize world, URIWithoutFinalize uri) {
     return rasqal_feature_from_uri(world.handle, uri.handle);
 }
 
-// TODO: Stopped at Feature_Value_Type
+FeatureValueType getType(FeatureType feature) {
+    return cast(FeatureValueType)rasqal_feature_value_type(feature);
+}
+
+// TODO: Stopped at Feature_Description
