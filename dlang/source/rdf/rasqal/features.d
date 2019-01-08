77c5a6049e2869e28808cdbf6491dec598c094c2
Victor Porton <porton@narod.ru>
2019-01-08 21:22:06 +0200

    started work on Rasqal features

--- dlang/source/rdf/rasqal/features.d
+++ dlang/source/rdf/rasqal/features.d
@@ -1,5 +1,16 @@
 module rdf.rasqal.features;
 
+import rdf.raptor.uri;
+import rdf.rasqal.world;
+
 enum FeatureType { Unknown = -1, No_Net = 0, Rand_Seed = 1 }
 
-// TODO: Stopped at Feature_From_URI
+private extern extern(C) {
+    FeatureType rasqal_feature_from_uri(RasqalWorldHandle* world, URIHandle* uri);
+}
+
+FeatureType Feature_From_URI(RasqalWorldWithoutFinalize world, URIWithoutFinalize uri) {
+    return rasqal_feature_from_uri(world.handle, uri.handle);
+}
+
+// TODO: Stopped at Feature_Value_Type
