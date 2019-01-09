b6552ca9b02efc30949d1d56f63ef4bd4be861fe
Victor Porton <porton@narod.ru>
2019-01-09 13:06:07 +0200

    rasqal/syntax.d implemented

--- dlang/source/rdf/rasqal/syntax.d
+++ dlang/source/rdf/rasqal/syntax.d
@@ -11,18 +11,59 @@ private extern extern(C) {
     const(SyntaxDescription*) rasqal_world_get_query_results_format_description(
         RasqalWorldHandle* world,
         uint counter);
+    int rasqal_language_name_check(RasqalWorldHandle* world, const char *name);
 }
 
-ref const(SyntaxDescription) getQueryLanguageDescription(RasqalWorldWithoutFinalize world,
-                                                        uint counter)
+const(SyntaxDescription*) getQueryLanguageDescription(RasqalWorldWithoutFinalize world,
+                                                      uint counter)
 {
-    return *rasqal_world_get_query_language_description(world.handle, counter);
+    return rasqal_world_get_query_language_description(world.handle, counter);
 }
 
-ref const(SyntaxDescription) getQueryResultsFormatDescription(RasqalWorldWithoutFinalize world,
+const(SyntaxDescription*) getQueryResultsFormatDescription(RasqalWorldWithoutFinalize world,
                                                              uint counter)
 {
-    return *rasqal_world_get_query_results_format_description(world.handle, counter);
+    return rasqal_world_get_query_results_format_description(world.handle, counter);
 }
 
-// TODO: Stopped at Query_Language_Description_Cursor
+struct QueryLanguageIterator {
+private:
+    RasqalWorldWithoutFinalize _world;
+    uint _counter = 0;
+public:
+    this(RasqalWorldWithoutFinalize world) {
+        _world = world;
+    }
+    bool empty() {
+        return !getQueryLanguageDescription(_world, _counter);
+    }
+    ref const(SyntaxDescription) front() {
+        return *getQueryLanguageDescription(_world, _counter);
+    }
+    void popFront() {
+        ++_counter;
+    }
+}
+
+struct QueryResultsFormatIterator {
+private:
+    RasqalWorldWithoutFinalize _world;
+    uint _counter = 0;
+public:
+    this(RasqalWorldWithoutFinalize world) {
+        _world = world;
+    }
+    bool empty() {
+        return !getQueryResultsFormatDescription(_world, _counter);
+    }
+    ref const(SyntaxDescription) front() {
+        return *getQueryResultsFormatDescription(_world, _counter);
+    }
+    void popFront() {
+        ++_counter;
+    }
+}
+
+bool languageNameCheck(RasqalWorldWithoutFinalize world, string name) {
+    return rasqal_language_name_check(world.handle, name.toStringz) != 0;
+}
