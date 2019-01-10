0d057c9fcd039912cdea2fead03eba1c71ad1e8a
Victor Porton <porton@narod.ru>
2019-01-10 18:59:35 +0200

    is*() functions for query results

--- dlang/source/rdf/rasqal/query_results.d
+++ dlang/source/rdf/rasqal/query_results.d
@@ -4,14 +4,41 @@ import rdf.auxiliary.handled_record;
 
 struct QueryResultsHandle;
 
+enum QueryResultsType { Bindings,
+                        Boolean,
+                        Graph,
+                        Syntax,
+                        Unknown }
+
 private extern extern(C) {
     void rasqal_free_query_results(QueryResultsHandle* query_results);
+    QueryResultsType rasqal_query_results_get_type(QueryResultsHandle* query_results);
+    int rasqal_query_results_is_bindings(QueryResultsHandle* query_results);
+    int rasqal_query_results_is_boolean(QueryResultsHandle* query_results);
+    int rasqal_query_results_is_graph(QueryResultsHandle* query_results);
+    int rasqal_query_results_is_syntax(QueryResultsHandle* query_results);
 }
 
 struct QueryResultsWithoutFinalize {
     mixin WithoutFinalize!(QueryResultsHandle,
                            QueryResultsWithoutFinalize,
                            QueryResults);
+    @property QueryResultsType type() {
+        return rasqal_query_results_get_type(handle);
+    }
+    @property bool isBindings() {
+        return rasqal_query_results_is_bindings(handle) != 0;
+    }
+    @property bool isBoolean() {
+        return rasqal_query_results_is_boolean(handle) != 0;
+    }
+    @property bool isGraph() {
+        return rasqal_query_results_is_graph(handle) != 0;
+    }
+    @property bool isSyntax() {
+        return rasqal_query_results_is_syntax(handle) != 0;
+    }
+    // function addRow is deliberately not implemented
 }
 
 struct QueryResults {
@@ -21,5 +48,5 @@ struct QueryResults {
                         rasqal_free_query_results);
 }
 
-// TODO: Stopped at Query_Results_Type_Enum
+// TODO: Stopped at Finished
 
