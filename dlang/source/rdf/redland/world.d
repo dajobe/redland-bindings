80653eadab64216d745f40b71a4e8c2fb72bfae4
Victor Porton <porton@narod.ru>
2019-01-12 23:46:14 +0200

    world getters/setters

--- dlang/source/rdf/redland/world.d
+++ dlang/source/rdf/redland/world.d
@@ -1,6 +1,8 @@
 module rdf.redland.world;
 
 import rdf.auxiliary.handled_record;
+import rdf.raptor.world;
+import rdf.rasqal.world;
 
 struct RedlandWorldHandle;
 
@@ -8,6 +10,10 @@ private extern extern(C) {
     void librdf_free_world(RedlandWorldHandle* world);
     RedlandWorldHandle* librdf_new_world();
     void librdf_world_open(RedlandWorldHandle* world);
+    RaptorWorldHandle* librdf_world_get_raptor(RedlandWorldHandle* world);
+    RasqalWorldHandle* librdf_world_get_rasqal(RedlandWorldHandle* world);
+    void librdf_world_set_raptor(RedlandWorldHandle* world, RaptorWorldHandle* raptor_world_ptr);
+    void librdf_world_set_rasqal(RedlandWorldHandle* world, RasqalWorldHandle* rasqal_world_ptr);
 }
 
 enum {
@@ -22,6 +28,18 @@ struct RedlandWorldWithoutFinalize {
     void open() {
         librdf_world_open(handle);
     }
+    @property void rasqal(RasqalWorldWithoutFinalize rasqalWorld) {
+        librdf_world_set_rasqal(handle, rasqalWorld.handle);
+    }
+    @property RasqalWorldWithoutFinalize rasqal() {
+         return RasqalWorldWithoutFinalize.fromNonnullHandle(librdf_world_get_rasqal(handle));
+    }
+    @property void raptor(RaptorWorldWithoutFinalize raptorWorld) {
+        librdf_world_set_raptor(handle, raptorWorld.handle);
+    }
+    @property RaptorWorldWithoutFinalize raptor() {
+         return RaptorWorldWithoutFinalize.fromNonnullHandle(librdf_world_get_raptor(handle));
+    }
 }
 
 struct RedlandWorld {
@@ -32,5 +50,5 @@ struct RedlandWorld {
                         librdf_new_world);
 }
 
-// TODO: Stopped at Set_Rasqal
+// TODO: Stopped at Set_Digest
 
