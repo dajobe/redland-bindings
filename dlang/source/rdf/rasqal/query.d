47cb5a2d02919db5f6ba7c95ff22e43227a2d431
Victor Porton <porton@narod.ru>
2019-01-12 13:05:56 +0200

    started work on rdf.rasqal.query

--- dlang/source/rdf/rasqal/query.d
+++ dlang/source/rdf/rasqal/query.d
@@ -1,6 +1,27 @@
 module rdf.rasqal.query;
 
+import rdf.auxiliary.handled_record;
+
 // This API is partial, as I consider many C functions as internals.
+
+struct QueryHandle;
+
+private extern extern(C) {
+    void rasqal_free_query(QueryHandle* query);
+}
+
+struct QueryWithoutFinalize {
+    mixin WithoutFinalize!(QueryHandle,
+                           QueryWithoutFinalize,
+                           Query);
+}
+
+struct Query {
+    mixin WithFinalize!(QueryHandle,
+                        QueryWithoutFinalize,
+                        Query,
+                        rasqal_free_query);
+}
 
 // TODO
 
