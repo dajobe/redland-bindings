2cccb5b227b04ea666f3f795ade95dde96e8f3e0
Victor Porton <porton@narod.ru>
2019-01-12 18:13:54 +0200

    URI copy

--- dlang/source/rdf/redland/uri.d
+++ dlang/source/rdf/redland/uri.d
@@ -7,12 +7,14 @@ struct URIHandle;
 
 private extern extern(C) {
     void librdf_free_uri(URIHandle* uri);
+    URIHandle* librdf_new_uri_from_uri(URIHandle* old_uri);
 }
 
 struct URIWithoutFinalize {
     mixin WithoutFinalize!(URIHandle,
                            URIWithoutFinalize,
-                           URI);
+                           URI,
+                           librdf_new_uri_from_uri);
 }
 
 struct URI {
@@ -21,3 +23,6 @@ struct URI {
                         URI,
                         librdf_free_uri);
 }
+
+// TODO: Stopped at To_Raptor
+
