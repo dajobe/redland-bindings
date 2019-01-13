229370678c8f250e27181f9882ca934a367c4428
Victor Porton <porton@narod.ru>
2019-01-13 20:11:42 +0200

    to/from Raptor

--- dlang/source/rdf/redland/node.d
+++ dlang/source/rdf/redland/node.d
@@ -17,6 +17,9 @@ struct NodeWithoutFinalize {
                            NodeWithoutFinalize,
                            Node,
                            librdf_new_node_from_node);
+    @property rdf.raptor.term.Term toRaptor() { // FIXME: also dup() in Ada
+      return rdf.raptor.term.TermWithoutFinalize.fromHandle(cast(rdf.raptor.term.TermHandle*)handle).dup;
+    }
 }
 
 struct Node {
@@ -24,7 +27,10 @@ struct Node {
                         NodeWithoutFinalize,
                         Node,
                         librdf_free_node);
+    static Node fromRaptor(rdf.raptor.term.TermWithoutFinalize uri) { // FIXME: also dup() in Ada
+        return NodeWithoutFinalize.fromHandle(cast(NodeHandle*)uri.handle).dup;
+    }
 }
 
-// TODO: Stopped at To_Raptor
+// TODO: Stopped at Encode
 
