31cbb42c344134a01755af14b434f9fcb0c09757
Victor Porton <porton@narod.ru>
2019-01-13 22:54:44 +0200

    from/to Raptor

--- dlang/source/rdf/redland/statement.d
+++ dlang/source/rdf/redland/statement.d
@@ -1,6 +1,7 @@
 module rdf.redland.statement;
 
 import rdf.auxiliary.handled_record;
+static import rdf.raptor.statement;
 import rdf.redland.world;
 
 struct StatementHandle;
@@ -15,6 +16,10 @@ struct StatementWithoutFinalize {
                            StatementWithoutFinalize,
                            Statement,
                            librdf_new_statement_from_statement);
+    @property rdf.raptor.statement.Statement toRaptor() { // FIXME: also dup() in Ada
+      return rdf.raptor.statement.StatementWithoutFinalize.fromHandle(
+        cast(rdf.raptor.statement.StatementHandle*)handle).dup;
+    }
 }
 
 struct Statement {
@@ -22,7 +27,10 @@ struct Statement {
                         StatementWithoutFinalize,
                         Statement,
                         librdf_free_statement);
+    static Statement fromRaptor(rdf.raptor.statement.StatementWithoutFinalize uri) { // FIXME: also dup() in Ada
+        return StatementWithoutFinalize.fromHandle(cast(StatementHandle*)uri.handle).dup;
+    }
 }
 
-// Stopped at To_Raptor
+// Stopped at Statement_Part_Flags
 
