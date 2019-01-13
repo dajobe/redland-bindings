6bb3a32465c6a205903ebd07b0965a53064b91b4
Victor Porton <porton@narod.ru>
2019-01-13 20:25:30 +0200

    encode()

--- dlang/source/rdf/redland/node.d
+++ dlang/source/rdf/redland/node.d
@@ -2,6 +2,7 @@ module rdf.redland.node;
 
 import rdf.auxiliary.handled_record;
 static import rdf.raptor.term;
+import rdf.redland.world;
 
 struct NodeHandle;
 
@@ -10,6 +11,11 @@ alias NodeKind = rdf.raptor.term.TermKind;
 private extern extern(C) {
     void librdf_free_node(NodeHandle* node);
     NodeHandle* librdf_new_node_from_node(NodeHandle* node);
+    NodeHandle* librdf_node_decode(RedlandWorldHandle* world,
+                                   size_t *size_p,
+                                   char *buffer,
+                                   size_t length);
+    size_t librdf_node_encode(NodeHandle* node, char *buffer, size_t length);
 }
 
 struct NodeWithoutFinalize {
@@ -19,6 +25,12 @@ struct NodeWithoutFinalize {
                            librdf_new_node_from_node);
     @property rdf.raptor.term.Term toRaptor() { // FIXME: also dup() in Ada
       return rdf.raptor.term.TermWithoutFinalize.fromHandle(cast(rdf.raptor.term.TermHandle*)handle).dup;
+    }
+    string encode() {
+        size_t length = librdf_node_encode(handle, null, 0);
+        char[] buffer = new char[length];
+        cast(void)librdf_node_encode(handle, buffer.ptr, length);
+        return cast(string)buffer;
     }
 }
 
@@ -32,5 +44,5 @@ struct Node {
     }
 }
 
-// TODO: Stopped at Encode
+// TODO: Stopped at Equals
 
