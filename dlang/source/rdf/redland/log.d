99c8848e3b842f3e4d7750330373318cc9fdd2cb
Victor Porton <porton@narod.ru>
2019-01-14 14:34:56 +0200

    some properties

--- dlang/source/rdf/redland/log.d
+++ dlang/source/rdf/redland/log.d
@@ -1,6 +1,9 @@
 module rdf.redland.log;
 
-struct LogMessage;
+import std.string;
+import rdf.raptor.log;
+
+struct LogMessageHandle;
 
 enum LogLevel { None  = 0,
                 Debug = 1,
@@ -30,5 +33,26 @@ enum LogFacility { None,
                    Memory,
                    Raptor }
 
-// TODO: Stopped at Get_Code
+private extern extern(C) {
+    int librdf_log_message_code(LogMessageHandle* message);
+    LogLevel librdf_log_message_level(LogMessageHandle* message);
+    LogFacility librdf_log_message_facility(LogMessageHandle* message);
+    const(char*) librdf_log_message_message(LogMessageHandle* message);
+    LocatorHandle* librdf_log_message_locator(LogMessageHandle* message);
+}
+
+struct LogMessageType {
+private:
+    LogMessageHandle* handle;
+public:
+    @property int code() { return librdf_log_message_code(handle); }
+    @property int level() { return librdf_log_message_level(handle); }
+    @property LogFacility level() { return librdf_log_message_facility(handle); }
+    @property string message() {
+        return librdf_log_message_message(handle).fromStringz.idup;
+    }
+
+}
+
+// TODO: Stopped at Get_Locator
 
