687457d2e0e2240da7680b902a223125538154b3
Victor Porton <porton@narod.ru>
2019-01-14 21:14:05 +0200

    working

--- dlang/source/rdf/redland/query_results.d
+++ dlang/source/rdf/redland/query_results.d
@@ -1,17 +1,22 @@
 module rdf.redland.query_results;
 
 import rdf.auxiliary.handled_record;
+import rdf.redland.stream;
 
 struct QueryResultsHandle;
 
 private extern extern(C) {
     void librdf_free_query_results(QueryResultsHandle* query_results);
+    StreamHandle* librdf_query_results_as_stream(QueryResultsHandle* query_results);
 }
 
 struct QueryResultsWithoutFinalize {
     mixin WithoutFinalize!(QueryResultsHandle,
                            QueryResultsWithoutFinalize,
                            QueryResults);
+    Stream asStream() {
+        return Stream.fromNonnullHandle(librdf_query_results_as_stream(handle));
+    }
 }
 
 struct QueryResults {
@@ -21,4 +26,4 @@ struct QueryResults {
                         librdf_free_query_results);
 }
 
-// TODO: Stopped at As_Stream
+// TODO: Stopped at Get_Current_Count
