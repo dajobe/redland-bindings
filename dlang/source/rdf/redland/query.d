04bfee71dc30b9658ed0dd354d73fd8f056b7c1c
Victor Porton <porton@narod.ru>
2019-01-14 19:31:11 +0200

    started work on redland/query.d

---
+++ dlang/source/rdf/redland/query.d
@@ -0,0 +1,23 @@
+module rdf.redland.query;
+
+import rdf.auxiliary.handled_record;
+
+struct QueryHandle;
+
+private extern extern(C) {
+    void librdf_free_query(QueryHandle* query);
+}
+
+struct QueryWithoutFinalize {
+    mixin WithoutFinalize!(QueryHandle,
+                           QueryWithoutFinalize,
+                           Query);
+}
+
+struct Query {
+    mixin WithFinalize!(QueryHandle,
+                        QueryWithoutFinalize,
+                        Query,
+                        librdf_free_query);
+}
+
