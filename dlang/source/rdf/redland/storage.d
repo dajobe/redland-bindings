cd8a2460f4eaca4628425e3579e653f7c28c412b
Victor Porton <porton@narod.ru>
2019-01-16 00:36:20 +0200

    started work on storage

---
+++ dlang/source/rdf/redland/storage.d
@@ -0,0 +1,24 @@
+module rdf.redland.storage;
+
+import rdf.auxiliary.handled_record;
+
+struct StorageHandle;
+
+private extern extern(C) {
+    void librdf_free_storage(StorageHandle* storage);
+    StorageHandle* librdf_new_storage_from_storage(StorageHandle* old_storage);
+}
+
+struct StorageWithoutFinalize {
+    mixin WithoutFinalize!(StorageHandle,
+                           StorageWithoutFinalize,
+                           Storage,
+                           librdf_new_storage_from_storage);
+}
+
+struct Storage {
+    mixin WithFinalize!(StorageHandle,
+                        StorageWithoutFinalize,
+                        Storage,
+                        librdf_free_storage);
+}
