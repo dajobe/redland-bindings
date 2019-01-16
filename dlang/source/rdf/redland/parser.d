92b779c081443b516d987e922b8270a853e09569
Victor Porton <porton@narod.ru>
2019-01-16 14:16:02 +0200

    started work on Redland parser

---
+++ dlang/source/rdf/redland/parser.d
@@ -0,0 +1,22 @@
+module rdf.redland.parser;
+
+import rdf.auxiliary.handled_record;
+
+struct ParserHandle;
+
+private extern extern(C) {
+    void librdf_free_parser(ParserHandle* parser);
+}
+
+struct ParserWithoutFinalize {
+    mixin WithoutFinalize!(ParserHandle,
+                           ParserWithoutFinalize,
+                           Parser);
+}
+
+struct Parser {
+    mixin WithFinalize!(ParserHandle,
+                        ParserWithoutFinalize,
+                        Parser,
+                        librdf_free_parser);
+}
