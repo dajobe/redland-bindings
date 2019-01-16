c11ad212f50989cdda1ee72074843d1dc2257def
Victor Porton <porton@narod.ru>
2019-01-16 19:18:30 +0200

    working on parser.d

--- dlang/source/rdf/redland/parser.d
+++ dlang/source/rdf/redland/parser.d
@@ -1,7 +1,9 @@
 module rdf.redland.parser;
 
+import std.typecons;
 import std.string;
 import rdf.auxiliary.handled_record;
+import rdf.auxiliary.nullable_string;
 import rdf.redland.world;
 
 struct ParserHandle;
@@ -9,6 +11,10 @@ struct ParserHandle;
 private extern extern(C) {
     void librdf_free_parser(ParserHandle* parser);
     int librdf_parser_check_name(RedlandWorldHandle* world, const char *name);
+    const(char*) librdf_parser_guess_name2(RedlandWorldHandle* world,
+                                           const char *mime_type,
+                                           const char *buffer,
+                                           const char *identifier);
 }
 
 struct ParserWithoutFinalize {
@@ -28,5 +34,18 @@ bool parserCheckName(RedlandWorldWithoutFinalize world, string name) {
     return librdf_parser_check_name(world.handle, name.toStringz) != 0;
 }
 
-// TODO: Stopped at Parser_Guess_Name
+// Order of arguments not the same as in C
+string parserGuessName(RedlandWorld world,
+                       string mimeType,
+                       string identifier,
+                       Nullable!string buffer = Nullable!string())
+{
+    const char* result = librdf_parser_guess_name2(world.handle,
+                                                   mimeType.empty ? null : mimeType.ptr,
+                                                   buffer.myToStringz,
+                                                   identifier.empty ? null : identifier.ptr);
+    return result ? result.fromStringz.idup : "";
+}
+
+// TODO: Stopped at Get_Parser_Description
 
